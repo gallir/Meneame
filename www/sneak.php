24da8012d0c08148aa8cfa19a47d43c79fd73a95
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2006-11-25 18:48:18 +0000

    Quitado los "titles" de los iconos de votos en la fisgona.
    git-svn-id: file:///home/gallir/meneame/svn/src@8 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- www/sneak.php
+++ www/sneak.php
@@ -118,13 +118,13 @@ function to_html(data) {
 		case 'vote':
 			tooltip_ajax_call += " onmouseover=\"return tooltip.ajax_delayed(event, 'get_link.php', '"+data.id+"', 30000);\"";
 			if (data.status == '<? echo _('publicada');?>')
-				html += '<img src="img/common/sneak-vote-published01.png" width="20" height="16" alt="<?echo _('voto');?>" title="<?echo _('voto');?>" '+tooltip_ajax_call+'/><\/div>';
+				html += '<img src="img/common/sneak-vote-published01.png" width="20" height="16" alt="<?echo _('voto');?>" '+tooltip_ajax_call+'/><\/div>';
 			else
-				html += '<img src="img/common/sneak-vote01.png" width="20" height="16" alt="<?echo _('voto');?>" title="<?echo _('voto');?>" '+tooltip_ajax_call+'/><\/div>';
+				html += '<img src="img/common/sneak-vote01.png" width="20" height="16" alt="<?echo _('voto');?>"  '+tooltip_ajax_call+'/><\/div>';
 			break;
 		case 'problem':
 			tooltip_ajax_call += " onmouseover=\"return tooltip.ajax_delayed(event, 'get_link.php', '"+data.id+"', 30000);\"";
-			html += '<img src="img/common/sneak-problem01.png" width="20" height="16" alt="<?echo _('problema');?>" title="<?echo _('problema');?>" '+tooltip_ajax_call+'/><\/div>';
+			html += '<img src="img/common/sneak-problem01.png" width="20" height="16" alt="<?echo _('problema');?>" '+tooltip_ajax_call+'/><\/div>';
 			break;
 		case 'comment':
 			tooltip_ajax_call += " onmouseover=\"return tooltip.ajax_delayed(event, 'get_comment_tooltip.php', '"+data.id+"', 10000);\"";
