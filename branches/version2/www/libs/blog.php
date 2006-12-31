5795a97c2d816521e9fd56c98440bd1b29b86dd6
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2006-12-31 16:56:10 +0000

    Solucionado bug que cortaba el "path" en determinados blogs
    git-svn-id: file:///home/gallir/meneame/svn/src@260 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/libs/blog.php
+++ branches/version2/www/libs/blog.php
@@ -82,7 +82,7 @@ class Blog {
 			$rss_url['path'] = preg_replace('/\/$/', '', $rss_url['path']);
 			if($url_url['host'] == $rss_url['host']) {
 				$len = min(strlen($url_url['path']), strlen($rss_url['path']));
-				for($i=0;$i<$len;$i++) {
+				for($i=1;$i<=$len;$i++) {
 					if(substr($url_url['path'], 0, $i) != substr($rss_url['path'], 0, $i) ) {
 						break;
 					}
