afc490985731129a05fad4061a92cd56145b566f
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-01-01 12:37:31 +0000

    Versión buena y funcional. Conviene agregar un índice a la tabla links: ALTER TABLE `links` ADD INDEX ( `link_blog` )
    git-svn-id: file:///home/gallir/meneame/svn/src@264 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/scripts/check_blogs.php
+++ branches/version2/scripts/check_blogs.php
@@ -8,46 +8,34 @@ include(mnminclude.'link.php');
 header("Content-Type: text/plain");
 
 $blog = new Blog;
-$count = $db->get_var("SELECT count(*) from blogs where blog_url regexp 'http://.+/.+'");
+//$count = $db->get_var("SELECT count(*) from blogs where blog_url regexp 'http://.+/.+'");
+$count = $db->get_var("SELECT count(*) from blogs where blog_url regexp 'http://.+'");
 echo "$count <br>\n";
 flush();
-$ids = $db->get_col("SELECT blog_id from blogs where blog_url regexp 'http://.+/.+'");
+$ids = $db->get_col("SELECT blog_id from blogs where blog_url regexp 'http://.+' order by blog_id asc");
 foreach($ids as $dbid) {
-	$canditates = Array();
 	$blog->id = $dbid;
-	$blog->read();
+	if( !$blog->read()) 
+		continue;
 	$url = $db->get_var("select link_url from links where link_blog = $dbid limit 1");
-	$rss_a = array($blog->rss, $blog->rss2, $blog->atom);
-	foreach ($rss_a as $r) {
-		if (strlen($r) > 0) {
-			array_push($canditates, $r);
-		}
-	}
-	$r = $blog->shortest_text($canditates);
-	$url_url = parse_url($url);
-	$rss_url = parse_url($r);
-	if (!empty($r) && !empty($url) && $url_url['host'] == $rss_url['host']) {
-        // Try to find the base url
-		$path='';
-		$url_url['path'] = preg_replace('/\/$/', '', $url_url['path']);
-		$rss_url['path'] = preg_replace('/\/$/', '', $rss_url['path']);
-		if($url_url['host'] == $rss_url['host']) {
-			$len = min(strlen($url_url['path']), strlen($rss_url['path']));
-			for($i=1;$i<=$len;$i++) {
-				if(substr($url_url['path'], 0, $i) != substr($rss_url['path'], 0, $i) ) {
-					break;
-				}
-			$path = substr($url_url['path'], 0, $i);
+	if (!empty($url)) {
+		$old_url = $blog->url;
+		$blog->find_base_url($url);
+		$old_key = $blog->key;
+		$blog->calculate_key();
+		if ($blog->url != $old_url || $old_key != $blog->key) {
+			echo "NEW: $new_url ($old_url)<br>\n";
+			$old_id = $db->get_var("select blog_id from blogs where blog_key = '$blog->key' and blog_id != $blog->id");
+			if ($old_id > 0) {
+				echo "REPE: $old_id -> $blog->id<br>\n";
+				$db->query("update links set link_blog=$blog->id where link_blog=$old_id");
+				$db->query("delete from blogs where blog_id = $old_id");
 			}
-		}
-		$path = preg_replace('/\/$/', '', $path);
-		if(empty($url_url['scheme'])) $scheme="http";
-		else $scheme=$url_url['scheme'];
-		$new_url=$scheme.'://'.$url_url['host'].$path;
-		if ($new_url != $blog->url) {
-			echo "NEW: $new_url ($blog->url)<br>\n";
 			$blog->store();
 		}
+	} else {
+		echo "Deleting $dbid\n";
+		$db->query("delete from blogs where blog_id = $dbid");
 	}
 
 }
