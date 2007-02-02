b4c890cde2e218feeba20ae67712bc7412160cbd
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-02-02 16:18:53 +0000

    Arreglado bug.
    git-svn-id: file:///home/gallir/meneame/svn/src@420 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/story.php
+++ branches/version2/www/story.php
@@ -225,8 +225,7 @@ case 5:
 	include(mnmpath.'/libs/link_sneak.php');
 	echo '</fieldset>';
 	echo '</div>';
-	echo '<script type="text/javascript">$(function(){start_link_sneak()});</script>' . "\n"
-
+	echo '<script type="text/javascript">$(function(){start_link_sneak()});</script>' . "\n";
 	break;
 }
 // echo '<div class="story-vertical-completion">&nbsp</div>';
