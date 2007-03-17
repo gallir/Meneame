409dbfd85a6c26a740fa76aadcdd4027e21fc6c1
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-03-17 22:01:14 +0000

    Agregado un espacio al :grin:
    git-svn-id: file:///home/gallir/meneame/svn/src@562 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/sneak.php
+++ branches/version2/www/sneak.php
@@ -235,7 +235,7 @@ function put_smiley(str) {
 	str=str.replace(/(\s|^):-{0,1}\)/gi, ' <img src="img/smileys/smiley.gif" alt=":-)" title=":-)"/>');
 	str=str.replace(/(\s|^);-{0,1}\)/gi, ' <img src="img/smileys/wink.gif" alt=";)" title=";)" />');
 	str=str.replace(/(\s|^):-{0,1}&gt;/gi, ' <img src="img/smileys/cheesy.gif" alt=":->" title=":->" />');
-	str=str.replace(/(\s|^):-{0,1}D|:grin:/gi, '<img src="img/smileys/grin.gif" alt=":-D" title=":-D"/>');
+	str=str.replace(/(\s|^):-{0,1}D|:grin:/gi, ' <img src="img/smileys/grin.gif" alt=":-D" title=":-D"/>');
 	str=str.replace(/:oops:|&lt;:\(/gi, ' <img src="img/smileys/embarassed.gif" alt="&lt;&#58;(" title="&#58;oops&#58; &lt;&#58;(" />');
 	str=str.replace(/(\s|^)&gt;:-{0,1}\(/gi, ' <img src="img/smileys/angry.gif" alt="&gt;&#58;-(" title="&gt;&#58;-(" />');
 	str=str.replace(/(\s|^)\?(:-){0,1}\(/gi, ' <img src="img/smileys/huh.gif" alt="?(" title="?(" />');
@@ -245,7 +245,7 @@ function put_smiley(str) {
 	str=str.replace(/:roll:/gi, ' <img src="img/smileys/rolleyes.gif" alt=":roll:" title=":roll:" />');
 	str=str.replace(/(\s|^):-{0,1}P/gi, ' <img src="img/smileys/tongue.gif" alt=":-P" title=":-P" />');
 	str=str.replace(/(\s|^):-{0,1}x/gi, ' <img src="img/smileys/lipsrsealed.gif" alt=":-x" title=":-x" />');
-	str=str.replace(/(\s|^):-{0,1}\//gi, '$1 <img src="img/smileys/undecided.gif" alt=":-/" title=":-/ :/" />');
+	str=str.replace(/(\s|^):-{0,1}\//gi, ' <img src="img/smileys/undecided.gif" alt=":-/" title=":-/ :/" />');
 	str=str.replace(/(\s|^):'\(|:cry:/gi, ' <img src="img/smileys/cry.gif" alt=":\'(" title=":cry: :\'(" />');
 	str=str.replace(/(\s|^)[xX]D+|:lol:/g, ' <img src="img/smileys/laugh.gif" alt="xD" title=":lol: xD" />');
 	str=str.replace(/(\s|^):-{0,1}S/gi, ' <img src="img/smileys/confused.gif" alt=":-S" title=":-S :S"/>');
