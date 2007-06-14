598b124d97c48098d8920d18cc812cb5353eda4b
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-06-14 00:57:40 +0000

    EL backend.
    git-svn-id: file:///home/gallir/meneame/svn/src@730 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version2/www/backend/send_pingbacks.php
@@ -0,0 +1,51 @@
+<?php
+include('../config.php');
+include(mnminclude.'link.php');
+include(mnminclude.'trackback.php');
+
+
+if($_SERVER["SERVER_ADDR"] != $_SERVER["REMOTE_ADDR"]) {
+	syslog(LOG_NOTICE, "Remote address $_SERVER[REMOTE_ADDR] is no local address.");
+	echo "ein? $_SERVER[REMOTE_ADDR]\n";
+	die;
+}
+
+$linkid = (int) $_REQUEST['id'];
+if ($linkid <= 0) {
+	echo "no id";
+	die;
+}
+
+$link = new Link;
+$link->id = $linkid;
+if (!$link->read()) {
+	echo "error reading link\n";
+	die;
+}
+
+preg_match_all('/([\(\[:\.\s]|^)(https*:\/\/[^ \t\n\r\]\(\)\&]{5,70}[^ \t\n\r\]\(\)]*[^ .\t,\n\r\(\)\"\'\]\?])/i', $link->content, $matches);
+foreach ($matches[2] as $match) {
+	$tb = new Trackback;
+	$tb->link=clean_input_url($match);
+	$tb->link_id=$link->id;
+	$tb->author=$link->author;
+	if(!$tb->read()) {
+		echo "No está $match\n";
+		$tmp = new Link;
+		if(!$tmp->get($match, 2000)) {
+			echo "couldn't get $match\n";
+			next;
+		}
+		if(!$tmp->pingback()) {
+			echo "couldn't get pingback $match\n";
+			next;
+		}
+		$tb->link = clean_input_url($match);
+		$tb->url = clean_input_url($tmp->trackback);
+		$tb->send($link);
+		sleep (1);
+	} else {
+		next;
+	}
+}
+?>
