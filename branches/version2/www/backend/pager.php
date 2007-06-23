941ceef273732436eb2d88b06810efd941070c01
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-06-23 22:16:02 +0000

    Parche de Alberto Vidal
    git-svn-id: file:///home/gallir/meneame/svn/src@765 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/backend/pager.php
+++ branches/version2/www/backend/pager.php
@@ -7,30 +7,31 @@
 // 		http://www.affero.org/oagpl.html
 // AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
 // The code below was made by Beldar <beldar at gmail dot com>
+// Modified by Alberto Vidal <a24v7b at gmail dot com>
 
 function do_contained_pages($id, $total, $current, $page_size, $program, $type, $container) {
-	global $db;
 
 	$index_limit = 6;
 
 	$total_pages=ceil($total/$page_size);
 	$start=max($current-intval($index_limit/2), 1);
-	$end=$start+$index_limit-1;
+	$end=min($start+$index_limit-1, $total_pages);
+	$start=max($end-$index_limit+1,1);
 	
 	echo '<div class="mini-pages">';
 	if($start>1) {
 		$i = 1;
 		echo '<a href="javascript:get_votes(\''.$program.'\',\''.$type.'\',\''.$container.'\','.$i.','.$id.')" title="'._('ir a página')." $i".'">'.$i.'</a>';
-		echo '<span>...</span>';
+		if($start>2) echo '<span>...</span>';
 	}
-	for ($i=$start;$i<=$end && $i<= $total_pages;$i++) {
+	for ($i=$start;$i<=$end;$i++) {
 		if($i==$current) echo '<span class="current">';
 		echo '<a href="javascript:get_votes(\''.$program.'\',\''.$type.'\',\''.$container.'\','.$i.','.$id.')" title="'._('ir a página')." $i".'">'.$i.'</a>';
 		if($i==$current) echo '</span>';
 	}
 	if($total_pages>$end) {
 		$i = $total_pages;
-		echo '<span>...</span>';
+		if($total_pages>$end+1) echo '<span>...</span>';
 		echo '<a href="javascript:get_votes(\''.$program.'\',\''.$type.'\',\''.$container.'\','.$i.','.$id.')" title="'._('ir a página')." $i".'">'.$i.'</a>';
 	}
 	echo "</div>\n";
