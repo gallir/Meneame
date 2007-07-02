453aace1190b3bbe694c47e438116671daf92273
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-07-02 01:16:50 +0000

    Cambiado el css a mnm34
    git-svn-id: file:///home/gallir/meneame/svn/src@794 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/config.php
+++ branches/version2/www/config.php
@@ -87,7 +87,7 @@ $globals['karma_log'] = mnmpath . '/../../karma.log';
 
 
 // CSS files (main, color, notame)
-$globals['css_main'] = 'css/es/mnm33.css';
+$globals['css_main'] = 'css/es/mnm34.css';
 $globals['css_color'] = 'css/es/mnmcol.css';
 
 // This is for SMS messages
