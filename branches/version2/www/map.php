6cb2d52d79809c357d4c36e9522842d84c250269
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-07-03 17:48:10 +0000

    Primera versión funcionando del mapa con el xml.php.
    git-svn-id: file:///home/gallir/meneame/svn/src@806 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/map.php
+++ branches/version2/www/map.php
@@ -26,11 +26,48 @@ echo '<div id="map" style="width: 720px; height: 600px;margin:20px 0 0 20px"></d
 	function onLoad() {
 		if (geo_basic_load(false, false, 2)) {
 			geo_map.addControl(new GLargeMapControl());
+			geo_load_xml('link')
+			/*
 			var geoXml = new GGeoXml("http://<? echo get_server_name() . $globals['base_url'] ?>rss2.php?rows=100");
 			geo_map.addOverlay(geoXml);
+			*/
 		}
 	}
+
+var baseicon = new GIcon();
+baseicon.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png";
+baseicon.iconSize = new GSize(12, 20);
+baseicon.shadowSize = new GSize(22, 20);
+baseicon.iconAnchor = new GPoint(6, 20);
+baseicon.infoWindowAnchor = new GPoint(5, 1);
+
+var iconred = "http://labs.google.com/ridefinder/images/mm_20_red.png"
+var iconwhite = "http://labs.google.com/ridefinder/images/mm_20_white.png"
+var iconblue = "http://labs.google.com/ridefinder/images/mm_20_blue.png"
+var iconorange = "http://labs.google.com/ridefinder/images/mm_20_orange.png"
+var iconpurple = "http://labs.google.com/ridefinder/images/mm_20_purple.png"
+var iconyellow = "http://labs.google.com/ridefinder/images/mm_20_yellow.png"
+
+//icon.image = "http://labs.google.com/ridefinder/images/mm_20_red.png";
+
+function geo_load_xml(type) {
+	GDownloadUrl(base_url+"geo/xml.php?type="+type, function(data, responseCode) {
+		var xml = GXml.parse(data);
+		var markers = xml.documentElement.getElementsByTagName("marker");
+		for (var i = 0; i < markers.length; i++) {
+			var point = new GLatLng(parseFloat(markers[i].getAttribute("lat")),
+				parseFloat(markers[i].getAttribute("lng")));
+			var status = markers[i].getAttribute("status");
+			var icon = new GIcon(baseicon);
+			if (status == 'published') icon.image = iconred;
+			else if (status == 'queued') icon.image = iconyellow;
+			else if (status == 'discard') icon.image = iconpurple;
+			geo_map.addOverlay(new GMarker(point, icon));
+		}
+	});
+}
 </script>
+<?
 
 echo '</div>';
 echo '</div>';
