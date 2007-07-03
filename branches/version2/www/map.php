418e9039a858028245f9a75f2b008569b60bf394
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-07-03 20:35:47 +0000

    Segunda versión del mapa con el ajax funcionando.
    git-svn-id: file:///home/gallir/meneame/svn/src@807 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/map.php
+++ branches/version2/www/map.php
@@ -20,24 +20,13 @@ echo '<div id="container">'."\n";
 do_sidebar();
 echo '<div id="contents">';
 do_tabs('main', _('geo'), true);
-echo '<div id="map" style="width: 720px; height: 600px;margin:20px 0 0 20px"></div></div>';
+echo '<div id="map" style="width: 780px; height: 500px;margin:20px 0 0 20px"></div></div>';
 ?>
 <script type="text/javascript">
-	function onLoad() {
-		if (geo_basic_load(false, false, 2)) {
-			geo_map.addControl(new GLargeMapControl());
-			geo_load_xml('link')
-			/*
-			var geoXml = new GGeoXml("http://<? echo get_server_name() . $globals['base_url'] ?>rss2.php?rows=100");
-			geo_map.addOverlay(geoXml);
-			*/
-		}
-	}
-
 var baseicon = new GIcon();
-baseicon.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png";
+//baseicon.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png";
 baseicon.iconSize = new GSize(12, 20);
-baseicon.shadowSize = new GSize(22, 20);
+//baseicon.shadowSize = new GSize(22, 20);
 baseicon.iconAnchor = new GPoint(6, 20);
 baseicon.infoWindowAnchor = new GPoint(5, 1);
 
@@ -48,10 +37,28 @@ var iconorange = "http://labs.google.com/ridefinder/images/mm_20_orange.png"
 var iconpurple = "http://labs.google.com/ridefinder/images/mm_20_purple.png"
 var iconyellow = "http://labs.google.com/ridefinder/images/mm_20_yellow.png"
 
-//icon.image = "http://labs.google.com/ridefinder/images/mm_20_red.png";
+var geo_marker_mgr;
+	function onLoad() {
+		if (geo_basic_load(20, 10, 2)) {
+			geo_map.addControl(new GLargeMapControl());
+			geo_marker_mgr = new GMarkerManager(geo_map);
 
-function geo_load_xml(type) {
-	GDownloadUrl(base_url+"geo/xml.php?type="+type, function(data, responseCode) {
+			//if ( geo_marker_mgr.getMarkerCount(zoom) > 1) zoom=3;
+			geo_load_xml('link', 'published', 0);
+			geo_load_xml('link', 'queued', 3);
+			GEvent.addListener(geo_map, 'click', function (overlay, point) {
+				if (overlay && overlay.myId > 0) {
+					GDownloadUrl(base_url+"geo/"+overlay.type+".php?id="+overlay.myId, function(data, responseCode) {
+						overlay.openInfoWindowHtml(data);
+					});
+				}
+			});
+		}
+	}
+
+function geo_load_xml(type, status, zoom) {
+	GDownloadUrl(base_url+"geo/xml.php?type="+type+"&status="+status, function(data, responseCode) {
+		var batch = [];
 		var xml = GXml.parse(data);
 		var markers = xml.documentElement.getElementsByTagName("marker");
 		for (var i = 0; i < markers.length; i++) {
@@ -62,14 +69,17 @@ function geo_load_xml(type) {
 			if (status == 'published') icon.image = iconred;
 			else if (status == 'queued') icon.image = iconyellow;
 			else if (status == 'discard') icon.image = iconpurple;
-			geo_map.addOverlay(new GMarker(point, icon));
+			marker = new GMarker(point, icon);
+			marker.myId = parseInt(markers[i].getAttribute("id"));
+			marker.myType = type;
+			batch.push(marker);
 		}
+		geo_marker_mgr.addMarkers(batch, zoom);
+		geo_marker_mgr.refresh();
 	});
 }
 </script>
 <?
 
-echo '</div>';
-echo '</div>';
 do_footer();
 ?>
