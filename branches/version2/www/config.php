fee19bfc8c4719e9dd9b11d02c57c64dad6612a4
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-11-16 03:15:10 +0000

    Cambiado el nombre de los CSS, empezamos de 1.
    git-svn-id: file:///home/gallir/meneame/svn/src@1006 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/config.php
+++ branches/version2/www/config.php
@@ -108,8 +108,8 @@ $globals['karma_log'] = mnmpath . '/../../karma.log';
 
 
 // CSS files (main, color, notame)
-$globals['css_main'] = 'css/es/mnm35.css';
-$globals['css_color'] = 'css/es/mnmcol.css';
+$globals['css_main'] = 'css/es/mnm1.css';
+$globals['css_color'] = 'css/es/mnmcol1.css';
 
 // This is for SMS messages
 // Put your authorised addresses
