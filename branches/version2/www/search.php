99f35a30095b90b314aef2f5d4c6c2a7a0508141
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-12-18 23:02:43 +0000

    Corregido typo.
    git-svn-id: file:///home/gallir/meneame/svn/src@1110 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/www/search.php
+++ branches/version2/www/search.php
@@ -62,7 +62,7 @@ if ($response['ids']) {
 	foreach($response['ids'] as $link_id) {
 		$link->id=$link_id;
 		$link->read();
-		$link->print_summary('full', $link_status == 'published' ? 100 : 20);
+		$link->print_summary('full', $link->status == 'published' ? 100 : 20);
 	}
 }
 
