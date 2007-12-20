aa847db0d52722077b17ef43ccb697af13199006
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2007-12-20 21:41:47 +0000

    Simplificación bestial gracias a la nueva estructura de la bbdd.
    git-svn-id: file:///home/gallir/meneame/svn/src@1116 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/scripts/discard3.php
+++ branches/version2/scripts/discard3.php
@@ -7,14 +7,14 @@ header("Content-Type: text/plain");
 
 $now = time();
 $max_date = "date_sub(now(), interval 15 minute)";
-$min_date = "date_sub(now(), interval 20 hour)"; 
+$min_date = "date_sub(now(), interval 24 hour)"; 
 
 // Delete old bad links
 $from = $now - 1200;
 $db->query("delete from links where link_status='discard' and link_date < from_unixtime($from) and link_votes = 0");
 
 
-$negatives = $db->get_results("select SQL_NO_CACHE link_id, link_karma, link_votes, link_negatives, link_author from links where link_date < $max_date and link_date > $min_date and link_status = 'queued' and link_karma < link_votes*2 and link_negatives > 3 order by link_negatives desc limit 10 ");
+$negatives = $db->get_results("select SQL_NO_CACHE link_id, link_karma, link_votes, link_negatives, link_author from links where link_date < $max_date and link_date > $min_date and link_status = 'queued' and link_karma < link_votes*2 and link_negatives > 3 and link_negatives > (link_votes+1)");
 
 //$db->debug();
 if( !$negatives) { 
@@ -24,25 +24,10 @@ if( !$negatives) {
 
 foreach ($negatives as $negative) {
 	$linkid = $negative->link_id;
-
-	$positive_users_count = $db->get_var("select SQL_NO_CACHE count(*) from votes, users where vote_type='links' and vote_link_id = $linkid and vote_user_id > 0 and vote_value > 0 and user_id = vote_user_id and user_level != 'disabled'");	
-	$positive_users = intval($db->get_var("select SQL_NO_CACHE sum(vote_value) from votes, users where vote_type='links' and vote_link_id = $linkid and vote_user_id > 0 and vote_value > 0 and user_id = vote_user_id and user_level != 'disabled'"));	
-
-	$negative_users_count = $db->get_var("select SQL_NO_CACHE count(*) from votes, users where vote_type='links' and vote_link_id = $linkid and vote_user_id > 0 and vote_value < 0 and user_id = vote_user_id and user_level != 'disabled' and user_karma > 6");	
-	$negative_users = intval($db->get_var("select SQL_NO_CACHE sum(vote_value-user_karma/2) from votes, users where vote_type='links' and vote_link_id = $linkid and vote_user_id > 0 and vote_value < 0 and user_id = vote_user_id and user_level != 'disabled' and user_karma > 6"));	
-	
-	//if ($negative_users_count > 2 && ($negative_users_count + $negative_annonymous_count) > $positive_count &&
-//		$positive < abs($negative_annonymous) + abs($negative_users) ) {
-	if ($negative_users_count > 3 && $negative_users_count > $positive_users_count &&
-		$positive_users < /*abs($negative_annonymous) +*/ abs($negative_users) ) {
-			$status = "DISCARD";
-			$db->query("update links set link_status='discard' where link_id = $linkid");
-			// Add the discard to log/event
-			log_insert('link_discard', $linkid, $negative->link_author);
-	} else {
-			$status = "OK";
-	}
-	echo  "$linkid: $negative->link_karma ($negative->link_votes, $negative->link_negatives),  $negative_users ($negative_users_count) $status\n";
+	$db->query("update links set link_status='discard' where link_id = $linkid");
+	// Add the discard to log/event
+	log_insert('link_discard', $linkid, $negative->link_author);
+	echo  "$linkid: $negative->link_karma ($negative->link_votes, $negative->link_negatives)\n";
 
 }
 
