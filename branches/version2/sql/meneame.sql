84f012d151fd09ceecbb67645c92aead5baa10dc
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2008-06-26 18:20:50 +0000

    Nueva versión con la tabla de anotaciones.
    git-svn-id: file:///home/gallir/meneame/svn/src@1326 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/sql/meneame.sql
+++ branches/version2/sql/meneame.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: meneame
 -- ------------------------------------------------------
--- Server version	5.0.45-5-log
+-- Server version	5.0.51a-3-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,21 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `annotations`
+--
+
+DROP TABLE IF EXISTS `annotations`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `annotations` (
+  `annotation_key` char(40) NOT NULL,
+  `annotation_time` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `annotation_text` text NOT NULL,
+  PRIMARY KEY  (`annotation_key`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `avatars`
 --
 
 DROP TABLE IF EXISTS `avatars`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `avatars` (
   `avatar_id` int(11) NOT NULL,
   `avatar_image` blob NOT NULL,
   PRIMARY KEY  (`avatar_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `bans`
 --
 
 DROP TABLE IF EXISTS `bans`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `bans` (
   `ban_id` int(10) unsigned NOT NULL auto_increment,
   `ban_type` enum('email','hostname','punished_hostname','ip','words','proxy') NOT NULL,
@@ -41,12 +61,15 @@ CREATE TABLE `bans` (
   PRIMARY KEY  (`ban_id`),
   UNIQUE KEY `ban_type` (`ban_type`,`ban_text`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `blogs`
 --
 
 DROP TABLE IF EXISTS `blogs`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `blogs` (
   `blog_id` int(20) NOT NULL auto_increment,
   `blog_key` char(35) collate utf8_spanish_ci default NULL,
@@ -58,12 +81,15 @@ CREATE TABLE `blogs` (
   PRIMARY KEY  (`blog_id`),
   UNIQUE KEY `key` (`blog_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `categories`
 --
 
 DROP TABLE IF EXISTS `categories`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `categories` (
   `category__auto_id` int(11) NOT NULL auto_increment,
   `category_lang` char(4) collate utf8_spanish_ci NOT NULL default 'es',
@@ -76,12 +102,15 @@ CREATE TABLE `categories` (
   UNIQUE KEY `category_lang` (`category_lang`,`category_id`),
   UNIQUE KEY `id` (`category_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `chats`
 --
 
 DROP TABLE IF EXISTS `chats`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `chats` (
   `chat_time` int(10) unsigned NOT NULL default '0',
   `chat_uid` int(10) unsigned NOT NULL default '0',
@@ -90,12 +119,15 @@ CREATE TABLE `chats` (
   `chat_text` char(255) NOT NULL,
   KEY `chat_time` USING BTREE (`chat_time`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 MAX_ROWS=1000;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `comments`
 --
 
 DROP TABLE IF EXISTS `comments`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `comments` (
   `comment_id` int(20) NOT NULL auto_increment,
   `comment_type` enum('normal','admin','private') collate utf8_spanish_ci NOT NULL default 'normal',
@@ -115,12 +147,15 @@ CREATE TABLE `comments` (
   KEY `comment_user_id` (`comment_user_id`,`comment_date`),
   KEY `comment_link_id` (`comment_link_id`,`comment_order`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `favorites`
 --
 
 DROP TABLE IF EXISTS `favorites`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `favorites` (
   `favorite_user_id` int(10) unsigned NOT NULL,
   `favorite_link_id` int(10) unsigned NOT NULL,
@@ -128,12 +163,15 @@ CREATE TABLE `favorites` (
   UNIQUE KEY `favorite_user_id` (`favorite_user_id`,`favorite_link_id`),
   KEY `favorite_link_id` (`favorite_link_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `friends`
 --
 
 DROP TABLE IF EXISTS `friends`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `friends` (
   `friend_type` enum('affiliate','manual','hide') collate utf8_spanish_ci NOT NULL default 'affiliate',
   `friend_from` int(10) NOT NULL default '0',
@@ -142,12 +180,15 @@ CREATE TABLE `friends` (
   UNIQUE KEY `friend_type` (`friend_type`,`friend_from`,`friend_to`),
   KEY `friend_type_2` (`friend_type`,`friend_to`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `geo_links`
 --
 
 DROP TABLE IF EXISTS `geo_links`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `geo_links` (
   `geo_id` int(11) NOT NULL,
   `geo_text` char(80) default NULL,
@@ -155,12 +196,15 @@ CREATE TABLE `geo_links` (
   UNIQUE KEY `geo_id` (`geo_id`),
   SPATIAL KEY `geo_pt` (`geo_pt`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `geo_users`
 --
 
 DROP TABLE IF EXISTS `geo_users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `geo_users` (
   `geo_id` int(11) NOT NULL,
   `geo_text` char(80) default NULL,
@@ -168,24 +212,30 @@ CREATE TABLE `geo_users` (
   UNIQUE KEY `geo_id` (`geo_id`),
   SPATIAL KEY `geo_pt` (`geo_pt`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `languages`
 --
 
 DROP TABLE IF EXISTS `languages`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `languages` (
   `language_id` int(11) NOT NULL auto_increment,
   `language_name` varchar(64) collate utf8_spanish_ci NOT NULL default '',
   PRIMARY KEY  (`language_id`),
   UNIQUE KEY `language_name` (`language_name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `links`
 --
 
 DROP TABLE IF EXISTS `links`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `links` (
   `link_id` int(20) NOT NULL auto_increment,
   `link_author` int(20) NOT NULL default '0',
@@ -220,16 +270,19 @@ CREATE TABLE `links` (
   KEY `link_date` (`link_date`),
   KEY `link_status` (`link_status`,`link_date`,`link_category`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `logs`
 --
 
 DROP TABLE IF EXISTS `logs`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `logs` (
   `log_id` int(11) NOT NULL auto_increment,
   `log_date` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  `log_type` enum('link_new','comment_new','link_publish','link_discard','comment_edit','link_edit','post_new','post_edit','login_failed','spam_warn','link_geo_edit','user_new','user_delete') NOT NULL,
+  `log_type` enum('link_new','comment_new','link_publish','link_discard','comment_edit','link_edit','post_new','post_edit','login_failed','spam_warn','link_geo_edit','user_new','user_delete','link_depublished') NOT NULL,
   `log_ref_id` int(11) unsigned NOT NULL,
   `log_user_id` int(11) NOT NULL,
   `log_ip` char(24) character set utf8 collate utf8_spanish_ci default NULL,
@@ -238,24 +291,30 @@ CREATE TABLE `logs` (
   KEY `log_type` (`log_type`,`log_ref_id`),
   KEY `log_type_2` (`log_type`,`log_date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `pageloads`
 --
 
 DROP TABLE IF EXISTS `pageloads`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `pageloads` (
   `date` date NOT NULL,
   `type` enum('html','ajax','other','rss','image','api','sneaker','bot','geo') NOT NULL default 'html',
   `counter` int(11) NOT NULL default '0',
   PRIMARY KEY  (`date`,`type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `posts`
 --
 
 DROP TABLE IF EXISTS `posts`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `posts` (
   `post_id` int(11) unsigned NOT NULL auto_increment,
   `post_randkey` int(11) NOT NULL default '0',
@@ -271,47 +330,59 @@ CREATE TABLE `posts` (
   KEY `post_date` (`post_date`),
   KEY `post_user_id` (`post_user_id`,`post_date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `prefs`
 --
 
 DROP TABLE IF EXISTS `prefs`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `prefs` (
   `pref_user_id` int(11) NOT NULL,
   `pref_key` char(16) character set utf8 collate utf8_spanish_ci NOT NULL,
   `pref_value` char(6) character set utf8 collate utf8_spanish_ci NOT NULL,
   KEY `pref_user_id` (`pref_user_id`,`pref_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `sneakers`
 --
 
 DROP TABLE IF EXISTS `sneakers`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `sneakers` (
   `sneaker_id` char(24) NOT NULL,
   `sneaker_time` int(10) unsigned NOT NULL default '0',
   `sneaker_user` int(10) unsigned NOT NULL default '0',
   UNIQUE KEY `sneaker_id` (`sneaker_id`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 MAX_ROWS=1000;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `sph_counter`
 --
 
 DROP TABLE IF EXISTS `sph_counter`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `sph_counter` (
   `counter_id` int(11) NOT NULL,
   `max_doc_id` int(11) NOT NULL,
   PRIMARY KEY  (`counter_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `tags`
 --
 
 DROP TABLE IF EXISTS `tags`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `tags` (
   `tag_link_id` int(11) NOT NULL default '0',
   `tag_lang` char(4) collate utf8_spanish_ci NOT NULL default 'es',
@@ -320,12 +391,15 @@ CREATE TABLE `tags` (
   UNIQUE KEY `tag_link_id` (`tag_link_id`,`tag_lang`,`tag_words`),
   KEY `tag_lang` (`tag_lang`,`tag_date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `trackbacks`
 --
 
 DROP TABLE IF EXISTS `trackbacks`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `trackbacks` (
   `trackback_id` int(10) unsigned NOT NULL auto_increment,
   `trackback_link_id` int(11) NOT NULL default '0',
@@ -344,12 +418,15 @@ CREATE TABLE `trackbacks` (
   KEY `trackback_url` (`trackback_url`),
   KEY `trackback_date` (`trackback_date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `users` (
   `user_id` int(20) NOT NULL auto_increment,
   `user_login` char(32) collate utf8_spanish_ci NOT NULL,
@@ -377,14 +454,19 @@ CREATE TABLE `users` (
   KEY `user_email` (`user_email`),
   KEY `user_karma` (`user_karma`),
   KEY `user_public_info` (`user_public_info`),
-  KEY `user_phone` (`user_phone`)
+  KEY `user_phone` (`user_phone`),
+  KEY `user_date` (`user_date`),
+  KEY `user_modification` (`user_modification`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `votes`
 --
 
 DROP TABLE IF EXISTS `votes`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `votes` (
   `vote_id` int(20) NOT NULL auto_increment,
   `vote_type` enum('links','comments','posts') character set utf8 NOT NULL default 'links',
@@ -398,12 +480,15 @@ CREATE TABLE `votes` (
   KEY `vote_type_2` (`vote_type`,`vote_user_id`),
   KEY `vote_type_4` (`vote_type`,`vote_date`,`vote_user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci PACK_KEYS=0;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `votes_summary`
 --
 
 DROP TABLE IF EXISTS `votes_summary`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `votes_summary` (
   `votes_year` smallint(4) NOT NULL,
   `votes_month` tinyint(2) NOT NULL,
@@ -412,6 +497,7 @@ CREATE TABLE `votes_summary` (
   `votes_count` int(11) NOT NULL,
   UNIQUE KEY `votes_year` (`votes_year`,`votes_month`,`votes_type`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+SET character_set_client = @saved_cs_client;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -422,4 +508,4 @@ CREATE TABLE `votes_summary` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-03-24  0:32:43
+-- Dump completed on 2008-06-26 18:19:45
