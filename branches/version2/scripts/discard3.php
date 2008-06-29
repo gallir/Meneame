d3337de4dc6e0d4ef6dd04de9845ece5809f8cb3
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2008-06-29 14:13:34 +0000

    Agregada función para penalizar a autores de comentarios con muchos negativos
    git-svn-id: file:///home/gallir/meneame/svn/src@1334 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version2/scripts/discard3.php
+++ branches/version2/scripts/discard3.php
@@ -19,6 +19,8 @@ $db->query("delete from links where link_status='discard' and link_date > date_s
 // send back to queue links with too many negatives
 $links = $db->get_results("select link_id, link_author, link_date, link_karma from links where link_status = 'published' and link_date > date_sub(now(), interval 3 hour) and link_negatives > link_votes / 5");
 
+punish_comments();
+
 if ($links) {
 	foreach ($links as $link) {
 		// count those votes with $globals['negative_votes_values'] = -8 =>  _('errónea'),  -9 => _('copia/plagio')
@@ -72,4 +74,59 @@ foreach ($negatives as $negative) {
 
 }
 
+
+function punish_comments($hours = 6) {
+	global $globals, $db;
+
+
+	$log = new Annotation('punish-comment');
+	if ($log->read() && $log->time > time() - 86400*$hours) {
+		echo "Comments already verified at: " . get_date_time($log->time) . "\n";
+		return false;
+	}
+
+	if ($globals['min_karma_for_comments'] > 0) $min_karma =  $globals['min_karma_for_comments'];
+	else $min_karma =  4.5;
+
+	$votes_from = time() - $hours * 3600; // 'date_sub(now(), interval 6 hour)';
+	$comments_from =  time() - 2 * $hours * 3600; //'date_sub(now(), interval 12 hour)';
+
+
+	echo "Starting karma_comments...\n";
+
+	$users = "SELECT SQL_NO_CACHE distinct comment_user_id as user_id from comments, users where comment_date > from_unixtime($comments_from) and comment_karma < -50 and comment_user_id = user_id and user_level != 'disabled' and user_karma >= $min_karma";
+	$result = $db->get_results($users);
+
+	$log->store();
+	if (! $result) return;
+
+	foreach ($result as $dbuser) {
+		$user = new User;
+		$user->id=$dbuser->user_id;
+		$user->read();
+		printf ("%07d  %s\n", $user->id, $user->username);
+		$punish = 0;
+	
+		$comment_votes_count = (int) $db->get_var("SELECT SQL_NO_CACHE count(*) from votes, comments where comment_user_id = $user->id and comment_date > from_unixtime($comments_from) and vote_type='comments' and vote_link_id = comment_id and  vote_date > from_unixtime($votes_from) and vote_user_id != $user->id");
+		if ($comment_votes_count > 5)  {
+			$votes_karma = (int) $db->get_var("SELECT SQL_NO_CACHE sum(vote_value) from votes, comments where comment_user_id = $user->id and comment_date > from_unixtime($comments_from) and vote_type='comments' and vote_link_id = comment_id and vote_date > from_unixtime($votes_from) and vote_user_id != $user->id");
+			if ($votes_karma < 50) {
+			 	$distinct_votes_count = (int) $db->get_var("SELECT SQL_NO_CACHE count(distinct comment_id) from votes, comments where comment_user_id = $user->id and comment_date > from_unixtime($comments_from) and vote_type='comments' and vote_link_id = comment_id and  vote_date > from_unixtime($votes_from) and vote_user_id != $user->id");
+				$comments_count = (int) $db->get_var("SELECT SQL_NO_CACHE count(*) from comments where comment_user_id = $user->id and comment_date > from_unixtime($comments_from)");
+				$comment_coeff =  min($comments_count/10, 1) * min($distinct_votes_count/($comments_count*0.75), 1);
+				$punish = max(-2, round($votes_karma * $comment_coeff * 1/1000,2));
+			}
+		}
+		if ($punish < -0.1) {
+			echo "comments: $comments_count votes distinct: $distinct_votes_count karma: $votes_karma coef: $comment_coeff -> $punish\n";
+			$user->karma += $punish;
+			//$user->store();
+			$annotation = new Annotation("karma-$user->id");
+			//$annotation->append(_('Penalización por comentarios').": $punish, nuevo karma: $user->karma\n");
+			echo(_('Penalización por comentarios').": $punish, nuevo karma: $user->karma\n");
+			$log->append(_('Penalización')." $user->username: $punish, nuevo karma: $user->karma\n");
+		}
+		$db->barrier();
+	}
+}
 ?>
