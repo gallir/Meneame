6fbf8288d5c712f4baad28e276659be58c7e0dcf
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2008-07-27 18:21:49 +0000

    Actualizado a la última versión en el menéame
    git-svn-id: file:///home/gallir/meneame/svn/src@1514 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/sql/categories.sql
+++ branches/version3/sql/categories.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: meneame
 -- ------------------------------------------------------
--- Server version	5.0.41-Debian_1~bpo.1-log
+-- Server version	5.0.51a-3-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,6 +20,8 @@
 --
 
 DROP TABLE IF EXISTS `categories`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `categories` (
   `category__auto_id` int(11) NOT NULL auto_increment,
   `category_lang` char(4) collate utf8_spanish_ci NOT NULL default 'es',
@@ -31,7 +33,8 @@ CREATE TABLE `categories` (
   PRIMARY KEY  (`category__auto_id`),
   UNIQUE KEY `category_lang` (`category_lang`,`category_id`),
   UNIQUE KEY `id` (`category_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=66 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=71 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `categories`
@@ -39,7 +42,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'es',1,100,'software libre',NULL,1.12088),(4,'es',4,100,'internet','internet',1.12088),(6,'es',6,101,'blogs',NULL,1.12318),(38,'es',38,102,'sociedad','sociedad',0.823064),(14,'es',8,100,'hardware',NULL,1.12088),(16,'es',22,100,'ciencia',NULL,1.12088),(19,'es',13,100,'diseño',NULL,1.12088),(22,'es',11,100,'software',NULL,1.12088),(27,'es',19,100,'cacharros',NULL,1.12088),(29,'es',23,100,'juegos',NULL,1.12088),(33,'es',32,101,'friqui',NULL,1.12318),(57,'es',28,101,'podcast','podcast',1.12318),(35,'es',35,101,'curiosidades',NULL,1.12318),(36,'es',36,101,'derechos',NULL,1.12318),(37,'es',37,100,'seguridad',NULL,1.12088),(39,'es',5,101,'cine/TV',NULL,1.12318),(40,'es',100,0,'tecnología','tecnologia',1.12088),(41,'es',101,0,'cultura','cultura',1.12318),(42,'es',102,0,'actualidad','actualidad',0.823064),(43,'es',7,100,'empresas',NULL,1.12088),(44,'es',9,101,'música',NULL,1.12318),(45,'es',10,101,'vídeos','videos',1.12318),(46,'es',12,101,'espectáculos','espectaculos',1.12318),(47,'es',15,101,'historia','historia',1.12318),(48,'es',16,101,'literatura','literatura',1.12318),(49,'es',17,102,'américas','americas',0.823064),(50,'es',18,102,'europa','europa',0.823064),(51,'es',20,102,'internacional','internacional',0.823064),(53,'es',24,102,'política','politica',0.823064),(54,'es',25,102,'economía','economía',0.823064),(55,'es',26,102,'última hora','ultima-hora',0.823064),(56,'es',27,102,'deportes','deportes',0.823064),(58,'es',29,101,'educación','educación',1.12318),(59,'es',39,100,'medicina','medicina',1.12088),(60,'es',40,100,'energía','energia',1.12088),(61,'es',41,101,'arte','arte',1.12318),(62,'es',42,100,'novedades','novedades-tec',1.12088),(63,'es',43,100,'medio ambiente','medio-ambiente',1.12088),(64,'es',44,102,'personalidades','personalidades',0.823064),(65,'es',45,102,'prensa','prensa',0.823064);
+INSERT INTO `categories` VALUES (1,'es',1,100,'software libre',NULL,1.04252),(4,'es',4,100,'internet','internet',1.04252),(6,'es',6,103,'blogs',NULL,0.787306),(38,'es',38,102,'sociedad','sociedad',1.2186),(14,'es',8,100,'hardware',NULL,1.04252),(16,'es',22,101,'ciencia',NULL,1.06563),(19,'es',13,100,'diseño',NULL,1.04252),(22,'es',11,100,'software',NULL,1.04252),(70,'es',64,102,'sucesos','sucesos',1.2186),(29,'es',23,100,'juegos',NULL,1.04252),(33,'es',32,103,'friqui',NULL,0.787306),(57,'es',28,103,'podcast','podcast',0.787306),(35,'es',35,103,'curiosidades',NULL,0.787306),(36,'es',36,101,'derechos',NULL,1.06563),(37,'es',37,100,'seguridad',NULL,1.04252),(39,'es',5,103,'TV','tv',0.787306),(40,'es',100,0,'tecnología','tecnologia',1.04252),(41,'es',101,0,'cultura','cultura',1.06563),(42,'es',102,0,'actualidad','actualidad',1.2186),(43,'es',7,102,'empresas',NULL,1.2186),(44,'es',9,101,'música',NULL,1.06563),(45,'es',10,103,'vídeos','videos',0.787306),(46,'es',12,103,'espectáculos','espectaculos',0.787306),(47,'es',15,101,'historia','historia',1.06563),(48,'es',16,101,'literatura','literatura',1.06563),(49,'es',17,102,'américas','americas',1.2186),(50,'es',18,102,'europa','europa',1.2186),(51,'es',20,102,'internacional','internacional',1.2186),(53,'es',24,102,'política','politica',1.2186),(54,'es',25,102,'economía','economía',1.2186),(56,'es',27,103,'deportes','deportes',0.787306),(58,'es',29,101,'educación','educación',1.06563),(59,'es',39,100,'medicina','medicina',1.04252),(60,'es',40,100,'energía','energia',1.04252),(61,'es',41,101,'arte','arte',1.06563),(62,'es',42,100,'novedades','novedades-tec',1.04252),(63,'es',43,100,'medioambiente','medioambiente',1.04252),(64,'es',44,102,'personalidades','personalidades',1.2186),(65,'es',45,101,'prensa','prensa',1.06563),(66,'es',103,0,'ocio','ocio',0.787306),(67,'es',60,101,'fotografía','fotografia',1.06563),(68,'es',61,101,'divulgación','divulgacion',1.06563),(69,'es',62,101,'cine','cine',1.06563);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2007-08-31 16:56:19
+-- Dump completed on 2008-07-27 18:21:13
