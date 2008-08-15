972702035613854fef14979a7b747e604a5fcd69
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2008-08-15 20:24:15 +0000

    Agregado icono de rss
    git-svn-id: file:///home/gallir/meneame/svn/src@1526 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/rss2.php
+++ branches/version3/www/rss2.php
@@ -270,7 +270,7 @@ function do_header($title) {
 	echo'	<title>'.$title.'</title>'."\n";
 	echo '	<atom:link href="http://'.get_server_name().htmlentities(clean_input_url($_SERVER['REQUEST_URI'])).'" rel="self" type="application/rss+xml" />'."\n";
 	echo'	<link>http://'.get_server_name().$home.'</link>'."\n";
-	echo"	<image><title>$title</title><link>http://".get_server_name().$home."</link><url>http://".get_server_name().$globals['base_url']."img/mnm/logo01-rss.gif</url></image>\n";
+	echo"	<image><title>$title</title><link>http://".get_server_name().$home."</link><url>http://".get_server_name().$globals['base_url']."img/mnm/eli-rss.png</url></image>\n";
 	echo'	<description>'._('Sitio colaborativo de publicación y comunicación entre blogs').'</description>'."\n";
 	echo'	<pubDate>'.date("r", $last_modified).'</pubDate>'."\n";
 	echo'	<generator>http://blog.meneame.net/</generator>'."\n";
