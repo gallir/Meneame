eae6c3a49ba1e265d3bf18e0a4713fb2e34c9457
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2008-09-06 17:59:23 +0000

    Actualización de código de los css
    git-svn-id: file:///home/gallir/meneame/svn/src@1542 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/config.php
+++ branches/version3/www/config.php
@@ -118,8 +118,8 @@ $globals['karma_log'] = mnmpath . '/../../karma.log';
 
 
 // CSS files (main, color, notame)
-$globals['css_main'] = 'css/es/mnm02.css';
-$globals['css_color'] = 'css/es/mnmcol01.css';
+$globals['css_main'] = 'css/es/mnm03.css';
+$globals['css_color'] = 'css/es/mnmcol03.css';
 
 // Store access stats
 $globals['save_pageloads'] = false;
