3996960a9312fe9e195220be64813cce553c96e1
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-01-01 16:11:56 +0000

    Cambiado el nombre del js
    git-svn-id: file:///home/gallir/meneame/svn/src@1695 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/libs/html1-mobile.php
+++ branches/version3/www/libs/html1-mobile.php
@@ -141,7 +141,7 @@ function do_js_includes() {
 	global $globals;
 
 	echo '<script type="text/javascript">var base_url="'.$globals['base_url'].'";mobile_version = true;</script>'."\n";
-	echo '<script src="'.$globals['base_url'].'js/mobile01.js" type="text/javascript"></script>' . "\n";
+	echo '<script src="'.$globals['base_url'].'js/mobile02.js" type="text/javascript"></script>' . "\n";
 	do_js_from_array($globals['extra_js']);
 	echo '<script type="text/javascript">if(top.location != self.location)top.location = self.location;'."\n";
 	if ($globals['extra_js_text']) {
