9a0830542c3e1576c06d127a7bac1faebbb0528c
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-01-20 19:55:16 +0000

    Crea las conversaciones de los últimos 10 días con la fecha real del comentario.
    git-svn-id: file:///home/gallir/meneame/svn/src@1763 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/scripts/conversations.php
+++ branches/version3/scripts/conversations.php
@@ -5,7 +5,7 @@ include(mnminclude.'comment.php');
 header("Content-Type: text/plain");
 
 $db->connect();
-$sql = "select comment_id from comments where comment_date > date_sub(now(), interval 7 day) and comment_content like '%#%'";
+$sql = "select comment_id from comments where comment_date > date_sub(now(), interval 10 day) and comment_content like '%#%'";
 $result = mysql_query($sql, $db->dbh) or die('Query failed: ' . mysql_error());
 while ($res = mysql_fetch_object($result)) {
 	$comment = new Comment;
@@ -13,7 +13,7 @@ while ($res = mysql_fetch_object($result)) {
 	$comment->read();
 	echo "Updating $comment->id\n";
 	$comment->update_conversation();
-	usleep(100);
+	usleep(1000);
 }
 
 
