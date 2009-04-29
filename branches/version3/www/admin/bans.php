ef0bab016095b5ce99d6b8cbd77ba7222bc88235
benjami <benjami@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-04-29 15:37:25 +0000

    modificación de emergencia
    git-svn-id: file:///home/gallir/meneame/svn/src@1850 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/admin/bans.php
+++ branches/version3/www/admin/bans.php
@@ -68,7 +68,7 @@ function admin_bans($ban_type) {
 	global $db, $globals, $offset, $page_size, $ban_text_length, $ban_comment_length, $current_user;
 	require_once(mnminclude.'ban.php');
 
-	$key = get_security_key();
+	if (!empty($key)) $key = get_security_key();
 	if ($current_user->user_level=="god" && $_REQUEST["key"] == $key) {
 		if (!empty($_REQUEST["new_ban"])) {
 			insert_ban($ban_type, $_POST["ban_text"], $_POST["ban_comment"], $_POST["ban_expire"]);
