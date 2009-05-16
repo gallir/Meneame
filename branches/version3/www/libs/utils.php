59f9eda4d0f2cea31dd198230d7d6c6dd16699bc
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-05-16 11:10:07 +0000

    Usa el abs() para las fechas en el futuro, sólo mostraba la hora en esos casos
    git-svn-id: file:///home/gallir/meneame/svn/src@1894 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/libs/utils.php
+++ branches/version3/www/libs/utils.php
@@ -240,7 +240,7 @@ function get_date($epoch) {
 function get_date_time($epoch) {
 		global $globals;
 	    //return date("Y-m-d H:i", $epoch);
-		if ($globals['now'] - $epoch < 43200) // Difference is less than 12 hours
+		if (abs($globals['now'] - $epoch) < 43200) // Difference is less than 12 hours
 	    	return date(" H:i e", $epoch);
 		else
 	    	return date(" d-m-Y H:i e", $epoch);
