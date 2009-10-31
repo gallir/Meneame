02e69cb829deff631c31311c201b987aa606652f
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-10-31 22:14:28 +0000

    Agregado comentario HTML al JS para que valide correctamente
    git-svn-id: file:///home/gallir/meneame/svn/src@2037 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/libs/charts.php
+++ branches/version3/www/libs/charts.php
@@ -12,26 +12,25 @@ function chart_link_karma_history($link_id) {
 	echo '<div id="flot" style="width:500px;height:250px;"></div>'."\n";
 
 	echo '<script src="'.$globals['base_static'].'js/jquery.flot.min.js" type="text/javascript"></script>'."\n";
-	echo '<script type="text/javascript">';
+	echo '<script type="text/javascript">'."\n//<!--\n";
 	echo '$(function () {'."\n";
 	echo 'var options = {'."\n";
 	echo 'lines: { show: true },'."\n";
 	echo 'points: { show: true },'."\n";
 	echo 'legend: { position: "nw" },'."\n";
 	echo 'xaxis: { mode: "time", minTickSize: [1, "hour"], }'."\n";
-	//echo 'xaxis: {tickDecimals: 0, tickSize: 1 }'."\n";
 	echo '};'."\n";
 	echo 'var data = [];'."\n";
 	echo 'var placeholder = $("#flot");'."\n";
 	echo '$.plot(placeholder, data, options);'."\n";
 
-	echo '$.getJSON("'.$globals['base_url'].'backend/karma-story.json?id='.$link_id.'", function (json) {';
+	echo '$.getJSON("'.$globals['base_url'].'backend/karma-story.json.php?id='.$link_id.'", function (json) {';
 	echo 'for (i=0; i<json.length; i++) { data.push(json[i]); }';
 	echo '$.plot(placeholder, data, options);';
 	echo '});';
 
 
 	echo '});'."\n";
-	echo '</script>'."\n";
+	echo "//-->\n</script>\n";
 }
 ?>
