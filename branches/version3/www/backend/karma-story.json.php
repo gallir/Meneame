72cf5bbdb0781642559310f891e4ca524a8fd21a
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-10-31 22:09:06 +0000

    Agregado el backend para el chart de logs de links
    git-svn-id: file:///home/gallir/meneame/svn/src@2036 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version3/www/backend/karma-story.json.php
@@ -0,0 +1,51 @@
+<?
+// The source code packaged with this file is Free Software, Copyright (C) 2009 by
+// Ricardo Galli <gallir at gallir dot com>.
+// It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
+// You can get copies of the licenses here:
+// 		http://www.affero.org/oagpl.html
+// AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
+
+
+include('../config.php');
+include(mnminclude.'link.php');
+
+header('Content-Type: text/x-json; charset=utf-8');
+//header('Content-Type: text/plain; charset=utf-8');
+
+if (empty($_GET['id'])) die;
+$id = intval($_GET['id']);
+$link = new Link;
+$link->id=$id;
+$link->read();
+if(!$link->read) die;
+if ( ($array = $link->read_annotation("link-karma")) != false ) {
+	$data['positives'] = array();
+	$data['negatives'] = array();
+	$data['anonymous'] = array();
+	$data['karma'] = array();
+
+	$array = array_reverse($array);
+	foreach ($array as $log) {
+		foreach (array_keys($data) as $key) {
+			$item = array($log['time']*1000, $log[$key]);
+			array_push($data[$key], $item);
+		}
+	}
+}
+// Generate the JSON array
+echo "[\n";
+foreach (array_keys($data) as $key) {
+	echo "{\n";
+	echo "label: '$key',\n";
+	if ($key == 'karma') echo "yaxis: 2,\n";
+	echo "data: [";
+	foreach ($data[$key] as $d) {
+		echo "[$d[0], $d[1]], ";
+	}
+	echo "]\n";
+	echo "},\n";
+}
+echo "]\n";
+?>
+
