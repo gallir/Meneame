b194d1e41f960c0faff65c508123c02cc34ddbf1
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2009-11-09 22:10:39 +0000

    Separadas las funciones de post en Twitter y Jaiku
    git-svn-id: file:///home/gallir/meneame/svn/src@2054 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version3/www/libs/external_post.php
@@ -0,0 +1,92 @@
+<?
+// The source code packaged with this file is Free Software, Copyright (C) 2009 by
+// Ricardo Galli <gallir at uib dot es>.
+// It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
+// You can get copies of the licenses here:
+// 		http://www.affero.org/oagpl.html
+// AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
+
+
+function twitter_post($link, $short_url) {
+	global $globals;
+
+	$t_status = urlencode(text_sub_text($link->title, 115) . ' ' . $short_url);
+	syslog(LOG_NOTICE, "Meneame: twitter updater called, id=$link->id");
+	$t_url = "http://twitter.com/statuses/update.xml";
+
+	if (!function_exists('curl_init')) {
+		syslog(LOG_NOTICE, "Meneame: curl is not installed");
+		return;
+	}
+	$session = curl_init();
+	curl_setopt($session, CURLOPT_URL, $t_url);
+	curl_setopt($session, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($session, CURLOPT_HEADER, false);
+	curl_setopt($session, CURLOPT_USERAGENT, "meneame.net");
+	curl_setopt($session, CURLOPT_CONNECTTIMEOUT, 15);
+	curl_setopt($session, CURLOPT_USERPWD, $globals['twitter_user'] . ":" . $globals['twitter_password']);
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($session, CURLOPT_POST, 1);
+	curl_setopt($session, CURLOPT_POSTFIELDS,"status=" . $t_status);
+	$result = curl_exec($session);
+	curl_close($session);
+}
+
+
+function jaiku_post($link, $short_url) {
+	global $globals;
+
+	syslog(LOG_NOTICE, "Meneame: jaiku updater called, id=$link->id");
+	$url = "http://api.jaiku.com/json";
+
+	if (!function_exists('curl_init')) {
+		syslog(LOG_NOTICE, "Meneame: curl is not installed");
+		return;
+	}
+
+
+	$postdata =  "method=presence.send";
+	$postdata .= "&user=" . urlencode($globals['jaiku_user']);
+	$postdata .= "&personal_key=" . $globals['jaiku_key'];
+	$postdata .= "&icon=337"; // Event
+	$postdata .= "&message=" . urlencode(text_sub_text(html_entity_decode($link->title), 115). ' ' . $short_url);
+
+	$session = curl_init();
+	curl_setopt($session, CURLOPT_URL, $url);
+	curl_setopt($session, CURLOPT_HEADER, false);
+	curl_setopt($session, CURLOPT_USERAGENT, "meneame.net");
+	curl_setopt($session, CURLOPT_CONNECTTIMEOUT, 15);
+	curl_setopt($session, CURLOPT_TIMEOUT, 20);
+	curl_setopt ($session, CURLOPT_FOLLOWLOCATION,1); 
+	curl_setopt($session, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($session, CURLOPT_POST, 1);
+	curl_setopt($session, CURLOPT_POSTFIELDS,$postdata);
+	$result = curl_exec($session);
+	curl_close($session);
+}
+
+function fon_gs($url) {
+	$gs_url = 'http://fon.gs/create.php?url='.urlencode($url);
+	$res = get_url($gs_url);
+	if ($res && $res['content'] && preg_match('/^OK/', $res['content'])) {
+		$array = explode(' ', $res['content']);
+		return $array[1];
+	} else {
+		return $url;
+	}
+}
+
+function pubsub_post() {
+	require_once(mnminclude.'pubsubhubbub/publisher.php');
+	global $globals;
+
+	if (! $globals['pubsub']) return false;
+	$rss = 'http://'.get_server_name().$globals['base_url'].'rss2.php';
+	$p = new Publisher($globals['pubsub']);
+	if ($p->publish_update($rss)) {
+		syslog(LOG_NOTICE, "Meneame: posted to pubsub ($rss)");
+	} else {
+		syslog(LOG_NOTICE, "Meneame: failed to post to pubsub ($rss)");
+	}
+}
+?>
