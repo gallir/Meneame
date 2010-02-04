cbbaa86ced14931aaadf8a5c59c865f07745d112
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-02-04 19:48:42 +0000

    Nuevo sistema de autentificación con firma en el "user"
    git-svn-id: file:///home/gallir/meneame/svn/src@2171 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/libs/login.php
+++ branches/version3/www/libs/login.php
@@ -32,26 +32,20 @@ class UserAuth {
 
 		if($this->mnm_user[0] && !empty($_COOKIE['mnm_key'])) {
 			$userInfo=explode(":", base64_decode($_COOKIE['mnm_key']));
-			if($this->mnm_user[0] === $userInfo[0]) {
-				$cookietime = (int) $userInfo[3];
-				$dbusername = $db->escape($this->mnm_user[0]);
-				$user=$db->get_row("SELECT SQL_CACHE user_id, user_pass as md5_pass, user_level, UNIX_TIMESTAMP(user_validated_date) as user_date, user_karma, user_email, user_avatar, user_comment_pref FROM users WHERE user_login = '$dbusername'");
+			if($this->mnm_user[0] == $userInfo[0]) {
+				$cookietime = intval($userInfo[3]);
+				if (($this->now - $cookietime) > 864000) $cookietime = 'expired'; // after 10 days expiration is forced
 
-				// We have two versions from now
-				// The second is more strong agains brute force md5 attacks
 				switch ($userInfo[2]) {
-					case '3':
-						if (($this->now - $cookietime) > 864000) $cookietime = 'expired'; // after 10 days expiration is forced
-						$key = md5($user->user_email.$site_key.$dbusername.$user->user_id.$cookietime);
-						break;
-					case '2':
-						$key = md5($user->user_email.$site_key.$dbusername.$user->user_id);
-						$cookietime = 0;
+					case '4':
+						$user_id = intval($this->mnm_user[0]);
+						$user=$db->get_row("SELECT SQL_CACHE user_id, user_login, user_pass as md5_pass, user_level, UNIX_TIMESTAMP(user_validated_date) as user_date, user_karma, user_email, user_avatar, user_comment_pref FROM users WHERE user_id = $user_id");
 						break;
 					default:
-						$key = md5($site_key.$dbusername.$user->user_id);
-						$cookietime = 0;
+						$dbusername = $db->escape($this->mnm_user[0]);
+						$user=$db->get_row("SELECT SQL_CACHE user_id, user_login, user_pass as md5_pass, user_level, UNIX_TIMESTAMP(user_validated_date) as user_date, user_karma, user_email, user_avatar, user_comment_pref FROM users WHERE user_login = '$dbusername'");
 				}
+				$key = md5($user->user_email.$site_key.$user->user_login.$user->user_id.$cookietime);
 
 				if ( !$user || !$user->user_id > 0 || $key !== $userInfo[1] || 
 					$user->user_level == 'disabled' || $user->user_level == 'autodisabled' || 
@@ -61,13 +55,15 @@ class UserAuth {
 				}
 
 				foreach(get_object_vars($user) as $var => $value) $this->$var = $value;
-				$this->user_login  = $userInfo[0];
 				if ($this->user_level == 'admin' || $this->user_level == 'god') $this->admin = true;
-				$this->authenticated = TRUE;
+				$this->authenticated = true;
 
-				if ($userInfo[2] != '3') { // Update the cookie to version 3
+				if ($userInfo[2] != '4') { // Update the key
 					$this->SetIDCookie(2, true);
-				} elseif ($this->now - $cookietime > 3600) { // Update the time each hour
+					$this->SetUserCookie(true);
+				}
+
+				if ($this->now - $cookietime > 3600) { // Update the time each hour
 					$this->SetIDCookie(2, $userInfo[4] > 0 ? true : false);
 				}
 			}
@@ -86,16 +82,15 @@ class UserAuth {
 				$this->AddClone();
 				$this->SetUserCookie(true);
 			case 2: // Only update the key
-				// Atencion, cambiar aquí cuando se cambie el password de base de datos a MD5
 				if($remember) $time = $this->now + 3600000; // Valid for 1000 hours
 				else $time = 0;
 				$strCookie=base64_encode(
-						$this->user_login.':'
+						$this->user_id.':'
 						.md5($this->user_email.$site_key.$this->user_login.$this->user_id.$this->now).':'
-						.'3'.':' // Version number
+						.'4'.':' // Version number
 						.$this->now.':'
 						.$time);
-				setcookie("mnm_key", $strCookie, $time, $globals['base_url'].'; HttpOnly');
+				setcookie("mnm_key", $strCookie, $time, $globals['base_url'], null, null, true);
 				break;
 		}
 	}
@@ -137,22 +132,29 @@ class UserAuth {
 	function SetUserCookie($do_login) {
 		global $globals;
 		if ($do_login) {
-			setcookie("mnm_user", $this->user_login.':'.$this->mnm_user[1], $this->now + 3600000, $globals['base_url']);
+			$expiration = $this->now + 86400 * 100;
 		} else {
-			setcookie("mnm_user", '_:'.$this->mnm_user[1], $this->now + 360000, $globals['base_url']);
+			$expiration = $this->now + 86400 * 7;
+			$this->user_login = '_';
 		}
+		setcookie("mnm_user", 
+					$this->user_id.
+					':'.$this->mnm_user[1].
+					':'.$this->signature($this->user_id.$this->mnm_user[1]), 
+					$expiration, $globals['base_url'], null, null, true);
 	}
+
 	function AddClone() {
-			if (!empty($this->mnm_user[1])) {
-				$ids = explode("x", $this->mnm_user[1]);
-				while(count($ids) > 4) {
-					array_shift($ids);
-				}
-			} else {
-				$ids = array();
+		if (!empty($this->mnm_user[1])) {
+			$ids = explode("x", $this->mnm_user[1]);
+			while(count($ids) > 4) {
+				array_shift($ids);
 			}
-			array_push($ids, $this->user_id);
-			$this->mnm_user[1] = implode('x', $ids);
+		} else {
+			$ids = array();
+		}
+		array_push($ids, $this->user_id);
+		$this->mnm_user[1] = implode('x', $ids);
 	}
 
 	function GetClones() {
@@ -164,6 +166,19 @@ class UserAuth {
 			}
 		}
 		return $clones;
+	}
+
+	static function signature($str) {
+		global $site_key;
+		return substr(md5($str.$site_key), 0, 8);
+	}
+
+	static function valid_user_cookie() {
+		if(!empty($_COOKIE['mnm_user'])) {
+			$user=explode(":", $_COOKIE['mnm_user']);
+			return $user[2] == self::signature($user[0].$user[1]);
+		}
+		return false;
 	}
 }
 
