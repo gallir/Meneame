407c0839fc6e319c613d4d7082b76d3e5a9e03d9
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-04-13 21:09:16 +0000

    Agregado buzz para compartir
    git-svn-id: file:///home/gallir/meneame/svn/src@2244 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/libs/html1.php
+++ branches/version3/www/libs/html1.php
@@ -879,6 +879,8 @@ function print_share_icons($full_link, $short_link = false, $title = '') {
 	echo '&nbsp;&nbsp;<a href="http://twitter.com/home?status='.$short_link.'" target="_blank"><img src="'.$globals['base_static'].'img/favicons/twitter.gif" alt="twitter" title="'._('compartir en twitter').'" width="16" height="16"/></a>';
    	// Share it in Facebook
 	echo '&nbsp;&nbsp;<a href="http://www.facebook.com/share.php?u='.$short_link.'" target="_blank"><img src="'.$globals['base_static'].'img/favicons/fb.gif" alt="facebook" title="'._('compartir en facebook').'" width="16" height="16"/></a>';
+	// Share it in Buzz
+	echo '&nbsp;&nbsp;<a href="http://www.google.com/buzz/post?url='.$short_link.'" target="_blank"><img src="'.$globals['base_static'].'img/favicons/buzz.png" alt="buzz" title="'._('compartir en buzz').'" width="16" height="16"/></a>';
 	// Share it in Friendfeed
 	echo '&nbsp;&nbsp;<a href="http://friendfeed.com/?url='.$short_link.'&amp;title='.$title.'" target="_blank"><img src="'.$globals['base_static'].'img/favicons/ff.png" alt="friendfeed" title="'._('compartir en friendfeed').'" width="16" height="16"/></a>';
 }
