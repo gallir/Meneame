a72c33923b385302afe48e9b41552f750f0e7f1f
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-05-10 20:43:58 +0000

    Agregado ficheros de configuración de sphinx
    git-svn-id: file:///home/gallir/meneame/svn/src@2343 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/scripts/ip_clones.pl
+++ branches/version3/scripts/ip_clones.pl
@@ -5,28 +5,44 @@ use DBI;
 
 my $ip = shift;
 
+my $counter = 0;
+
 $ip =~ s/([0-9]\.[0-9]\.[0-9])\.[0]*$/$1/;
 my @integers = split /\./, $ip;
 
-if (scalar(@integers) < 3 || scalar(@integers) > 4) {
-	print "Usage: ip_clones.pl s.x.y[.z]\n";
+if (scalar(@integers) < 2 || scalar(@integers) > 4) {
+	print "Usage: ip_clones.pl s.x.[y[.z]]\n";
 	exit;
 }
 
-if (scalar(@integers) == 3) {
-	$sql = "select distinct user_login, user_email, inet_ntoa(vote_ip_int), user_level from votes, users where user_id=vote_user_id and vote_type in ('links','comments') and vote_date > date_sub(now(), interval 30 day) and vote_ip_int between inet_aton('$ip.0') and inet_aton('$ip.255');";
+if (scalar(@integers) <= 3) {
+	if (scalar(@integers) == 3) {
+		$cminor = "$ip.0";
+		$cmajor = "$ip.255";
+	} else {
+		$cminor = "$ip.0.0";
+		$cmajor = "$ip.255.255";
+	}
+	$sql = "select distinct user_login, user_email, inet_ntoa(vote_ip_int), user_level from votes, users where user_id=vote_user_id and vote_type in ('links','comments') and vote_ip_int between inet_aton('$cminor') and inet_aton('$cmajor');";
 } else {
-	$sql = "select distinct user_login, user_email, inet_ntoa(vote_ip_int), user_level from votes, users where user_id=vote_user_id and vote_type in ('links','comments') and vote_date > date_sub(now(), interval 30 day) and vote_ip_int = inet_aton('$ip');";
+	$sql = "select distinct user_login, user_email, inet_ntoa(vote_ip_int), user_level from votes, users where user_id=vote_user_id and vote_type in ('links','comments') and vote_ip_int = inet_aton('$ip');";
 }
 
-my $dbh = DBI->connect ('DBI:mysql:meneame', 'meneame', '');
+my $dbh = DBI->connect ('DBI:mysql:meneame;host=ec2-db-slave.meneame.net', 'meneame', '');
 
 my $sth = $dbh->prepare($sql);
 
 $sth->execute();
 while (($login, $email, $vote_ip, $level) = $sth->fetchrow_array ) {
 	print "$login\t$email\t$vote_ip\t$level\n";
+	$counter++;
 }
 
 $dbh->disconnect();
+
+if ($counter) {
+	exit(0);
+}
+
+exit (1);
 
