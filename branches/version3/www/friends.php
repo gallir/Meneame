7f8c3b5de3abd7d1c8d9abc3203f1778069ce4c3
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-05-29 20:23:28 +0000

    Added html generator for links voted by friends
    git-svn-id: file:///home/gallir/meneame/svn/src@2362 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version3/www/friends.php
@@ -0,0 +1,58 @@
+<?
+// The source code packaged with this file is Free Software, Copyright (C) 2005 by
+// Ricardo Galli <gallir at uib dot es>.
+// It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
+// You can get copies of the licenses here:
+// 		http://www.affero.org/oagpl.html
+// AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
+
+include('config.php');
+include(mnminclude.'html1.php');
+
+$globals['ads'] = true;
+
+$page_size = 20;
+$current_page = get_current_page();
+$offset=($current_page-1)*$page_size;
+
+if (!$current_user->user_id) {
+	header("Location: " . $globals['base_url']);
+	die;
+}
+
+
+$friends = $db->get_col("select friend_to from friends where friend_type = 'manual' and friend_from = $current_user->user_id and friend_value > 0");
+$friends_list = implode(',', $friends);
+$sql = "select distinct vote_link_id as link_id from votes where vote_type = 'links' and vote_user_id in ($friends_list) and vote_value > 0 order by vote_link_id desc";
+
+$links = $db->get_results("$sql LIMIT $offset,$page_size");
+
+do_header(_('votadas por amigos') . ' | men&eacute;ame');
+$globals['tag_status'] = 'published';
+do_tabs('main', 'friends');
+
+/*** SIDEBAR ****/
+echo '<div id="sidebar">';
+do_banner_right();
+do_banner_promotions();
+do_best_comments();
+do_vertical_tags('published');
+echo '</div>' . "\n";
+/*** END SIDEBAR ***/
+
+echo '<div id="newswrap">'."\n";
+
+if ($links) {
+	foreach($links as $dblink) {
+		$link = Link::from_db($dblink->link_id);
+		$link->do_inline_friend_votes = true;
+		$link->print_summary();
+	}
+}
+do_pages(-1, $page_size);
+echo '</div>'."\n";
+
+do_footer_menu();
+do_footer();
+
+?>
