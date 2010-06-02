fa273b4a8351d0a761313fc031d470ed23a62c71
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-06-02 18:43:19 +0000

    Agregado json
    git-svn-id: file:///home/gallir/meneame/svn/src@2371 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/api/url.php
+++ branches/version3/www/api/url.php
@@ -8,27 +8,56 @@
 
 include('../config.php');
 
-header('Content-Type: text/plain; charset=UTF-8');
+if (isset($_GET['json']))  {
+	$json = true;
+	$dict = array();
+	header('Content-Type: application/json; charset=utf-8');
+} else {
+	$json = false;
+	header('Content-Type: text/plain; charset=UTF-8');
+}
+
 stats_increment('api', true);
 
 $url = $db->escape($_GET['url']);
 $url = addcslashes($url, '%_');
 if(strlen($url) < 8 || ! ($p_url = parse_url($_GET['url'])) || strlen($p_url['host']) < 5) {
-	echo 'KO';
+	if ($json) {
+		$dict['status'] = 'KO';
+		echo json_encode($dict);
+	} else echo 'KO';
 	die;
 }
+
 $url = preg_replace('/\/$/', '', $url);
 $all = intval($_GET['all']);
 if ($all == '1') {
-    $links = $db->get_results("select SQL_NO_CACHE link_id, link_votes, link_anonymous, link_status from links where link_url like '$url%' order by link_date DESC limit 100");
+    $links = $db->get_results("select SQL_NO_CACHE link_id, link_votes, link_anonymous, link_negatives, link_status, link_karma from links where link_url like '$url%' order by link_date DESC limit 100");
 } else {
-    $links = $db->get_results("select SQL_NO_CACHE link_id, link_votes, link_anonymous, link_status from links where link_url in ('$url', '$url/')");
+    $links = $db->get_results("select SQL_NO_CACHE link_id, link_votes, link_anonymous, link_negatives, link_status, link_karma from links where link_url in ('$url', '$url/')");
 }
 if ($links) {
+	$dict['status'] = 'OK';
+	$dict['data'] = array();
 	foreach ($links as $dblink) {
-	    echo 'OK http://'.get_server_name().'/story.php?id='.$dblink->link_id.' '.($dblink->link_votes+$dblink->link_anonymous).' '.$dblink->link_status."\n";
+		if ($json) {
+			$data = array();
+			$data['url'] = 'http://'.get_server_name().'/story.php?id='.$dblink->link_id;
+			$data['status'] = $dblink->link_status;
+			$data['votes'] = intval($dblink->link_votes);
+			$data['anonymous'] = intval($dblink->link_anonymous);
+			$data['karma'] = intval($dblink->link_karma);
+			array_push($dict['data'], $data);
+	    } else {
+			echo 'OK http://'.get_server_name().'/story.php?id='.$dblink->link_id.' '.($dblink->link_votes+$dblink->link_anonymous).' '.$dblink->link_status."\n";
+		}
 	}
 } else {
-	echo 'KO http://'.get_server_name().'/submit.php?url='.$url;
+	if ($json) {
+		$dict['status'] = 'KO';
+		$dict['submit_url'] = 'http://'.get_server_name().'/submit.php?url='.$url;
+	} else echo 'KO http://'.get_server_name().'/submit.php?url='.$url;
 }
+
+if ($json) echo json_encode($dict);
 ?>
