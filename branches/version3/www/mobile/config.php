0a43b4adb12399470b59b7a47e69237272bf1faa
benjami <benjami@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-06-10 12:10:11 +0000

    Pequenio cambio en la cabecera de la version movil
    git-svn-id: file:///home/gallir/meneame/svn/src@2404 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/mobile/config.php
+++ branches/version3/www/mobile/config.php
@@ -34,7 +34,7 @@ $globals['base_user_url'] = 'user/';
 
 $globals['mobile_version'] = true; // Mark we are using mobile version
 
-$globals['css_main'] = 'css/es/mnm-mobile02.css';
+$globals['css_main'] = 'css/es/mnm-mobile03.css';
 $globals['css_color'] = '';
 $globals['html_main'] = 'html1-mobile.php';
 
