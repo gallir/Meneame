5befeff268f5592a54ae20a89977418d1ae27ff3
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-08-03 18:11:17 +0000

    Changed the icon for new post
    git-svn-id: file:///home/gallir/meneame/svn/src@2500 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version3/www/sneakme/index.php
+++ branches/version3/www/sneakme/index.php
@@ -296,7 +296,7 @@ function do_post_subheader($content, $selected = false, $rss = false, $rss_title
 
 	if ($current_user->user_id > 0 ) {
 		if (Post::can_add($current_user->user_id)) {
-			echo '<li class="selected"><span><a class="toggler" href="javascript:post_new()" title="'._('nueva nota').'">'._('nueva nota').'&nbsp;<img src="'.$globals['base_static'].'img/common/icon_add_post_001.png" alt="" width="10" height="12"/></a></span></li>';
+			echo '<li class="selected"><span><a class="toggler" href="javascript:post_new()" title="'._('nueva nota').'">'._('nueva nota').'&nbsp;<img src="'.$globals['base_static'].'img/common/icon_add_post_002.png" alt="" width="13" height="12"/></a></span></li>';
 		} else {
 			echo '<li><span><a href="javascript:return false;">'._('nueva nota').'</a></span></li>';
 		}
