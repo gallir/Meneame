83abaab38aeb1eeb35fef65dfba147fdc1f49b0f
crodas <crodas@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-08-23 07:29:46 +0000

    fixed little typo
    git-svn-id: file:///home/gallir/meneame/svn/src@2597 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/libs/Haanga/Extension/Filter/Hostname.php
+++ branches/version4/www/libs/Haanga/Extension/Filter/Hostname.php
@@ -4,6 +4,6 @@ class Haanga_Extension_Filter_Hostname
 {
     static function generator($cmp, $args)
     {
-        return hexec('parser_url', $args[0], hconst('PHP_URL_HOST'));
+        return hexec('parse_url', $args[0], hconst('PHP_URL_HOST'));
     }
 }
