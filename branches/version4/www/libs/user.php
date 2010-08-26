f0097f39095bc7e9e3b3c5518e90171cdf791c4e
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-08-26 22:56:09 +0000

    Don't allow ' and " in usernames (as allowed by Facebook)
    git-svn-id: file:///home/gallir/meneame/svn/src@2605 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/libs/user.php
+++ branches/version4/www/libs/user.php
@@ -21,7 +21,7 @@ class User {
 	static function get_valid_username($name) {
 		$name = strip_tags($name);
 		$name = preg_replace('/&.+?;/', '', $name); // kill entities
-		$name = preg_replace('/\s/', '_', $name); 
+		$name = preg_replace('/\s\'\"/', '_', $name); 
 		return substr($name, 0, 24);
 	}
 
