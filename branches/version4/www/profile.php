e5b34e3636c4a5e6354e9ed8577ae5845254385e
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-09-14 19:12:02 +0000

    Migrated profile to Haanga
    git-svn-id: file:///home/gallir/meneame/svn/src@2697 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/profile.php
+++ branches/version4/www/profile.php
@@ -69,158 +69,43 @@ if($current_user->user_id == $user->id && $globals['external_user_ads'] && !empt
 }
 
 
-
 if (isset($_POST['process'])) {
 	if (!check_form_auth_ip()) {
 		header("Location: http://".get_server_name().$globals['base_url']."profile.php");
 		die;
 	}
 	$globals['secure_page'] = True;
-	$save_messages = save_profile();
+	$messages = save_profile();
 } else {
 	$globals['secure_page'] = False;
+	$messages = array();
 }
 
 do_header(_('edición del perfil del usuario'). ': ' . $user->username);
-echo '<div id="singlewrap" style="margin: 0 40px; padding-top: 30px">'."\n";
-echo $save_messages; // We do it later because teh profile could change header's info
-show_profile();
-echo '</div>';
+
+
+//echo $save_messages; // We do it later because teh profile could change header's info
+//show_profile();
+
+$form = new stdClass;
+$form->hash = md5($site_key.$user->id.mnminclude);
+$form->admin_mode = $admin_mode;
+$form->auth_link = get_auth_link();
+$form->user_levels = $user_levels;
+$form->avatars_enabled = is_avatars_enabled();
+
+Haanga::Load('profile.html', compact('user', 'form', 'messages'));
+
+
 do_footer();
-
-
-function show_profile() {
-	global $user, $admin_mode, $user_levels, $globals, $site_key, $current_user;
-
-	echo '<div class="genericform" style="margin: 0 50px">';
-	echo '<form  enctype="multipart/form-data" action="'.get_auth_link().'profile.php" method="post" id="thisform" AUTOCOMPLETE="off">';
-	echo '<fieldset><legend>';
-	echo '<span class="sign">'._('opciones de usuario') . " <a href='".get_user_uri($user->username)."'>$user->username</a>: $user->level</span></legend>";
-
-	echo '<img class="thumbnail" src="'.get_avatar_url($user->id, $user->avatar, 80).'" width="80" height="80" alt="'.$user->username.'" />';
-	echo '<input type="hidden" name="process" value="1" />';
-	echo '<input type="hidden" name="user_id" value="'.$user->id.'" />';
-	echo '<input type="hidden" name="form_hash" value="'. md5($site_key.$user->id.mnminclude) .'" />';
-	get_form_auth_ip();
-	if ($admin_mode)
-		echo '<input type="hidden" name="login" value="'.$user->username.'" />';
-
-	echo '<p><label>'._('usuario').':</label><br/>';
-	echo '<input type="text" autocomplete="off" name="username" id="username" value="'.$user->username.'" onkeyup="enablebutton(this.form.checkbutton1, null, this)" />';
-	echo '&nbsp;&nbsp;<span id="checkit"><input type="button" class="button" id="checkbutton1" disabled="disabled" value="'._('verificar').'" onclick="checkfield(\'username\', this.form, this.form.username)"/></span>';
-	echo '&nbsp;<span id="usernamecheckitvalue"></span>' . "\n";
-	echo '</p>';
-
-	echo '<p><label>'._('nombre real').':</label><br/>';
-	echo '<input type="text" autocomplete="off" name="names" id="names" value="'.$user->names.'" />';
-	echo '</p>';
-
-	echo '<p><label>'._('correo electrónico').':</label><br/>';
-	echo '<input type="text" autocomplete="off" name="email" id="email" value="'.$user->email.'" onkeyup="enablebutton(this.form.checkbutton2, null, this)"/>';
-	echo '&nbsp;&nbsp;<input type="button" class="button" id="checkbutton2" disabled="disabled" value="'._('verificar').'" onclick="checkfield(\'email\', this.form, this.form.email)"/>';
-	echo '&nbsp;<span id="emailcheckitvalue"></span>';
-	echo '</p>';
-
-	echo '<p><label>'._('página web').':</label><br/>';
-	echo '<input type="text" autocomplete="off" name="url" id="url" value="'.$user->url.'" />';
-	echo '</p>';
-
-	echo '<p><label>'._('mensajero instantáneo público, invisible para los demás').':</label><br/>';
-	echo '<span class="note">' . _('necesario si te conectarás vía Jabber/Google Talk') . '</span><br/>';
-	echo '<input type="text" autocomplete="off" name="public_info" id="public_info" value="'.$user->public_info.'" />';
-	echo '</p>';
-
-	if ($user->id  == $current_user->user_id) {
-		echo '<p><label>'._('teléfono móvil').':</label><br/>';
-		echo '<span class="note">' . _('sólo necesario si enviarás notas al nótame vía SMS') . '</span><br/>';
-		echo '<span class="note">' . _('pon el número completo, con código de país: +34123456789') . '</span><br/>';
-		echo '<input type="text" autocomplete="off" name="phone" id="phone" value="'.$user->phone.'" />';
-		echo '</p>';
-	}
-
-
-
-	if ($globals['external_user_ads']) {
-		echo '<p><label for="adcode">'._('codigo AdSense').':</label><br/>';
-		echo '<span class="note">' . _('tu código de usuario de AdSense, del tipo pub-123456789') . '</span><br/>';
-		echo '<input type="text" autocomplete="off" name="adcode" id="adcode" maxlength="20" value="'.$user->adcode.'" /><br />';
-		echo '<span class="note">' . _('canal AdSense (opcional), del tipo 1234567890') . '</span><br/>';
-		echo '<input type="text" autocomplete="off" name="adchannel" id="adchannel" maxlength="12" value="'.$user->adchannel.'" />';
-		echo '</p>';
-	}
-
-
-	if (is_avatars_enabled()) {
-		echo '<input type="hidden" name="MAX_FILE_SIZE" value="400000" />';
-		echo '<p><label>'._('avatar').':</label><br/>';
-		echo '<span class="note">' . _('imagen cuadrada de no más de 400 KB, sin transparencias') . '</span><br/>';
-		echo '<input type="file" class="button" autocomplete="off" name="image" />';
-		if ($user->avatar > 0) {
-			echo '&nbsp;&nbsp;&nbsp;'._('Eliminar avatar').': <input type="checkbox" name="avatar_delete" value="1"/>';
-		}
-		echo '</p>';
-	}
-
-	echo '<fieldset><legend>'._('opciones de visualización') . '</legend>';
-	echo '<p><label>'._('mostrar todos los comentarios').':&nbsp;';
-	print_checkbox('comment_pref', $user->comment_pref & 1);
-	echo '</label></p>';
-
-	echo '</fieldset>';
-
-
-	
-	echo '<p>'._('introduce la nueva clave para cambiarla -no se cambiará si la dejas en blanco-:').'</p>';
-
-	echo '<p><label for="password">' . _("clave") . ':</label><br />' . "\n";
-	echo '<input type="password" autocomplete="off" id="password" name="password" size="25" onkeyup="return securePasswordCheck(this.form.password);"/></p>' . "\n";
-
-	echo '<p><label for="verify">' . _("repite la clave") . ': </label><br />' . "\n";
-	echo '<input type="password" autocomplete="off" id="verify" name="password2" size="25" onkeyup="checkEqualFields(this.form.password2, this.form.password)"/></p>' . "\n";
-
-	if ($admin_mode) {
-		echo '<p><label for="verify">' . _("estado") . ': </label><br />' . "\n";
-		echo '<select name="user_level">';
-		foreach ($user_levels as $level) {
-			echo '<option value="'.$level.'"';
-			if ($user->level == $level) echo ' selected="selected"';
-			echo '>'.$level.'</option>';
-		}
-		echo '</select>';
-
-		echo '<p><label for="karma">'._('karma').':</label><br/>';
-		echo '<input type="text" autocomplete="off" name="karma" id="karma" value="'.$user->karma.'" />';
-		echo '</p>';
-
-	}
-	
-	echo '<p><input type="submit" name="save_profile" value="'._('actualizar').'" class="button" /></p>';
-	echo '</fieldset>';
-
-	// Disable the account
-	if ($user->id  == $current_user->user_id) {
-		echo '<br/><fieldset><legend>'._('deshabilitar cuenta') . '</legend>';
-		echo '<p>'._('¡atención! la cuenta será deshabilitada.').'</p>';
-		echo '<p class="note">'._('se eliminarán automáticamente los datos personales.').'<br/>';
-		echo _('las notas serán eliminadas, los envíos y comentarios NO se borrarán.').'</p>';
-		echo '<p><label>'._('sí, quiero deshabilitarla').': <input  name="disable" type="checkbox" value="1"/>';
-		echo '</label></p>';
-		echo '<p><input type="submit" name="disabledme" value="'._('deshabilitar cuenta').'" class="button" /></p>';
-		echo '</fieldset>';
-	}
-
-
-	echo "</form></div>\n";
-	
-}
 
 
 function save_profile() {
 	global $db, $user, $current_user, $globals, $admin_mode, $site_key;
 	$errors = 0; // benjami: control added (2005-12-22)
 	$pass_changed=false;
-	$messages = '';
-	
+	$messages = array();
+
 	$form_hash = md5($site_key.$user->id.mnminclude);
 	if(isset($_POST['disabledme']) && intval($_POST['disable']) == 1 && $_POST['form_hash'] == $form_hash && $_POST['user_id'] == $current_user->user_id ) {
 		$old_user_login = $user->username;
@@ -234,11 +119,11 @@ function save_profile() {
 	}
 
 
-	if(!isset($_POST['save_profile']) || !isset($_POST['process']) || 
+	if(!isset($_POST['save_profile']) || !isset($_POST['process']) ||
 		($_POST['user_id'] != $current_user->user_id && !$admin_mode) ) return;
-		
+
 	if ( empty($_POST['form_hash']) || $_POST['form_hash'] != $form_hash ) {
-		$messages .= '<p class="form-error">'._('Falta la clave de control').'</p>';
+		array_push($messages, _('Falta la clave de control'));
 		$errors++;
 	}
 
@@ -246,26 +131,26 @@ function save_profile() {
 		$newname = trim($_POST['username']);
 
 		if (strlen($newname)<3) {
-			$messages .= '<p class="form-error">'._('nombre demasiado corto').'</p>';
+			array_push($messages, _('nombre demasiado corto'));
 			$errors++;
 		}
 
 		if(!check_username($newname)) {
-			$messages .= '<p class="form-error">'._('nombre de usuario erróneo, caracteres no admitidos').'</p>';
+			array_push($messages, _('nombre de usuario erróneo, caracteres no admitidos'));
 			$errors++;
 		} elseif (user_exists($newname, $user->id) ) {
-			$messages .= '<p class="form-error">'._('el usuario ya existe').'</p>';
+			array_push($messages, _('el usuario ya existe'));
 			$errors++;
 		} else {
 			$user->username=$newname;
 		}
 	}
-	
+
 	if($user->email != trim($_POST['email']) && !check_email(trim($_POST['email']))) {
-		$messages .= '<p class="form-error">'._('el correo electrónico no es correcto').'</p>';
+		array_push($messages, _('el correo electrónico no es correcto'));
 		$errors++;
 	} elseif (!$admin_mode && trim($_POST['email']) != $current_user->user_email && email_exists(trim($_POST['email']), false)) {
-		$messages .= '<p class="form-error">'. _('ya existe otro usuario con esa dirección de correo'). '</p>';
+		array_push($messages, _('ya existe otro usuario con esa dirección de correo'));
 		$errors++;
 	} else {
 		$user->email=trim($_POST['email']);
@@ -279,7 +164,7 @@ function save_profile() {
 		$public = $db->escape($_POST['public_info']);
 		$im_count = intval($db->get_var("select count(*) from users where user_id != $user->id and user_level != 'disabled' and user_level != 'autodisabled' and user_public_info='$public'"));
 		if ($im_count > 0) {
-			$messages .= '<p class="form-error">'. _('ya hay otro usuario con la misma dirección de MI, no se ha grabado'). '</p>';
+			array_push($messages, _('ya hay otro usuario con la misma dirección de MI, no se ha grabado'));
 			$_POST['public_info'] = '';
 			$errors++;
 		}
@@ -292,14 +177,14 @@ function save_profile() {
 		// Check phone number
 		if (!empty($_POST['phone'])) {
 			if ( !preg_match('/^\+[0-9]{9,16}$/', $_POST['phone'])) {
-				$messages .= '<p class="form-error">'. _('número telefónico erróneo, no se ha grabado'). '</p>';
+				array_push($messages, _('número telefónico erróneo, no se ha grabado'));
 				$_POST['phone'] = '';
 				$errors++;
 			} else {
 				$phone = $db->escape($_POST['phone']);
 				$phone_count = intval($db->get_var("select count(*) from users where user_id != $user->id and user_level != 'disabled' and user_level != 'autodisabled' and user_phone='$phone'"));
 				if ($phone_count > 0) {
-					$messages .= '<p class="form-error">'. _('ya hay otro usuario con el mismo número, no se ha grabado'). '</p>';
+					array_push($messages, _('ya hay otro usuario con el mismo número, no se ha grabado'));
 					$_POST['phone'] = '';
 					$errors++;
 				}
@@ -315,13 +200,13 @@ function save_profile() {
 		$_POST['adchannel'] = trim($_POST['adchannel']);
 		if (!empty($_POST['adcode']) && $user->adcode != $_POST['adcode']) {
 			if ( !preg_match('/^pub-[0-9]{16}$/', $_POST['adcode'])) {
-				$messages .= '<p class="form-error">'. _('código AdSense incorrecto, no se ha grabado'). '</p>';
+				array_push($messages, _('código AdSense incorrecto, no se ha grabado'));
 				$_POST['adcode'] = '';
 				$errors++;
 			} else {
 				$adcode_count = intval($db->get_var("select count(*) from users where user_id != $user->id and user_level != 'disabled' and user_level != 'autodisabled' and user_adcode='".$_POST['adcode']."'"));
 				if ($adcode_count > 0) {
-					$messages .= '<p class="form-error">'. _('ya hay otro usuario con la misma cuenta, no se ha grabado'). '</p>';
+					array_push($messages, _('ya hay otro usuario con la misma cuenta, no se ha grabado'));
 					$_POST['adcode'] = '';
 					$errors++;
 				}
@@ -329,7 +214,7 @@ function save_profile() {
 		}
 		if (!empty($_POST['adcode']) && !empty($_POST['adchannel']) && $user->adchannel != $_POST['adchannel']) {
 			if ( !preg_match('/^[0-9]{10,12}$/', $_POST['adchannel'])) {
-				$messages .= '<p class="form-error">'. _('canal AdSense incorrecto, no se ha grabado'). '</p>';
+				array_push($messages, _('canal AdSense incorrecto, no se ha grabado'));
 				$_POST['adchannel'] = '';
 				$errors++;
 			}
@@ -341,14 +226,14 @@ function save_profile() {
 	$user->names=clean_text($_POST['names']);
 	if(!empty($_POST['password']) || !empty($_POST['password2'])) {
 		if(! check_password($_POST["password"]) ) {
-    	    $messages .= '<p class="form-error">'._('Clave demasiado corta, debe ser de 6 o más caracteres e incluir mayúsculas, minúsculas y números').'</p>';
+    	    array_push($messages, _('Clave demasiado corta, debe ser de 6 o más caracteres e incluir mayúsculas, minúsculas y números'));
         	$errors=1;
 	    } else if(trim($_POST['password']) !== trim($_POST['password2'])) {
-			$messages .= '<p class="form-error">'._('las claves no son iguales, no se ha modificado').'</p>';
+			array_push($messages, _('las claves no son iguales, no se ha modificado'));
 			$errors = 1;
 		} else {
 			$user->pass=md5(trim($_POST['password']));
-			$messages .= '<p  class="form-error">'._('La clave se ha cambiado').'</p>';
+			$messages .= '<p  class="form-error">'._('La clave se ha cambiado');
 			$pass_changed = true;
 		}
 	}
@@ -366,14 +251,14 @@ function save_profile() {
 		if(avatars_check_upload_size('image')) {
 			$avatar_mtime = avatars_manage_upload($user->id, 'image');
 			if (!$avatar_mtime) {
-				$messages .= '<p class="form-error">'._('error guardando la imagen').'</p>';
+				array_push($messages, _('error guardando la imagen'));
 				$errors = 1;
 				$user->avatar = 0;
 			} else {
 				$user->avatar = $avatar_mtime;
 			}
 		} else {
-			$messages .= '<p class="form-error">'._('el tamaño de la imagen excede el límite').'</p>';
+			array_push($messages, _('el tamaño de la imagen excede el límite'));
 			$errors = 1;
 			$user->avatar = 0;
 		}
@@ -390,19 +275,13 @@ function save_profile() {
 		}
 		$user->store();
 		$user->read();
-		if (!$admin_mode && ($current_user->user_login != $user->username || 
+		if (!$admin_mode && ($current_user->user_login != $user->username ||
 					$current_user->user_email != $user->email || $pass_changed)) {
 			$current_user->Authenticate($user->username, $user->pass);
 		}
-		$messages .= '<p class="form-error">'._('datos actualizados').'</p>';
+		array_push($messages, _('datos actualizados'));
 	}
 	return $messages;
-}
-
-function print_checkbox($name, $current_value) {
-	echo '<input  name="'.$name.'" type="checkbox" value="1"'; 
-	if ($current_value > 0) echo '  checked="true"';
-	echo '/>';
 }
 
 ?>
