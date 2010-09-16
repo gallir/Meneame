f781e6937e0cda2f329b81a548e333382e6a48b0
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-09-16 00:42:46 +0000

    Check also for published
    git-svn-id: file:///home/gallir/meneame/svn/src@2711 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/backend/menealo.php
+++ branches/version4/www/backend/menealo.php
@@ -72,10 +72,12 @@ else $freq = 2;
 
 
 if ($link->status == 'published')  $freq *= 2; // Allow to play a little more if published
-elseif ($globals['click_counter'] 
+
+// Check for clicks vs votes
+// to avoid "cowboy votes" without reading the article
+if ($globals['click_counter'] 
 	&& ! $link->user_clicked() 
 	&& $link->total_votes > $link->get_clicks()) {
-	// Check there is more clicks than votes to avoid "cowboy votes"
 	error(_('no leído, y con más votos que lecturas').' ('.$link->get_clicks().' < '.$link->total_votes.')');
 }
 
