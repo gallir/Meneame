b0c219de8cbabdd6cc98e6f69ae8a087c6675843
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2010-12-09 10:49:34 +0000

    In gallery: changed tooltip for link. Use go.php as redirector to comments and posts based on ID. Added ajax reports in fancybox
    git-svn-id: file:///home/gallir/meneame/svn/src@2810 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/backend/go.php
+++ branches/version4/www/backend/go.php
@@ -10,26 +10,43 @@ include('../config.php');
 
 $id = intval($_GET['id']);
 if ($id > 0) {
-	$l = $db->get_row("select link_url as url, link_ip as ip from links where link_id = $id");
-	if ($l) {
-		header('HTTP/1.1 301 Moved');
-		header('Location: ' . $l->url);
-		header("Content-Length: 0");
-		header("Connection: close");
-		flush();
-
-		if (! $globals['bot'] 
-			&& $globals['click_counter']
-			&& isset($_COOKIE['k']) && check_security_key($_COOKIE['k'])
-			&& $l->ip != $globals['user_ip']
-			&& ! id_visited($id)) {
-			$db->query("INSERT LOW_PRIORITY INTO link_clicks (id, counter) VALUES ($id,1) ON DUPLICATE KEY UPDATE counter=counter+1");
-		}
-		exit(0);
+	$what = $_GET['what'];
+	switch ($what) {
+		case 'post':
+			$url = 'http://'.get_server_name().post_get_base_url($id);
+			do_redirection($url);
+			exit(0);
+		case 'comment':
+			$c = new Comment();
+			$c->id = $id;
+			$url = 'http://'.get_server_name().$c->get_relative_individual_permalink();
+			do_redirection($url);
+			exit(0);
+		default:
+			$l = $db->get_row("select link_url as url, link_ip as ip from links where link_id = $id");
+			if ($l) {
+				do_redirection($l->url);
+				if (! $globals['bot'] 
+					&& $globals['click_counter']
+					&& isset($_COOKIE['k']) && check_security_key($_COOKIE['k'])
+					&& $l->ip != $globals['user_ip']
+					&& ! id_visited($id)) {
+					$db->query("INSERT LOW_PRIORITY INTO link_clicks (id, counter) VALUES ($id,1) ON DUPLICATE KEY UPDATE counter=counter+1");
+				}
+				exit(0);
+			}
 	}
 }
 require(mnminclude.$globals['html_main']);
 do_error(_('enlace inexistente'), 404);
+
+function do_redirection($url) {
+	header('HTTP/1.1 301 Moved');
+	header('Location: ' . $url);
+	header("Content-Length: 0");
+	header("Connection: close");
+	flush();
+}
 
 function id_visited($id) {
 	if (! isset($_COOKIE['v']) || ! ($visited = preg_split('/x/', $_COOKIE['v'], 0, PREG_SPLIT_NO_EMPTY)) ) {
