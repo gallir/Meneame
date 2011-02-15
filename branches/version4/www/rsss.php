9771481d242ed364cf56efe9aa8f67de57cd1e5b
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-02-15 16:57:43 +0000

    Tabulated
    git-svn-id: file:///home/gallir/meneame/svn/src@2896 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/rsss.php
+++ branches/version4/www/rsss.php
@@ -12,7 +12,7 @@ include(mnminclude.'html1.php');
 $globals['ads'] = true;
 
 $rows = -1;
-$page_size = 50;
+$page_size = 25;
 $page = get_current_page();
 $offset=($page-1)*$page_size;
 
@@ -30,22 +30,27 @@ echo '</div>' . "\n";
 
 echo '<div id="newswrap">'."\n";
 
-
 echo '<div class="topheading"><h2>'._('apuntes de blogs').'</h2></div>';
+echo '<table class="decorated"><tr>';
 
-$entries = $db->get_results("select rss.blog_id, rss.user_id, title, url, user_login, user_avatar, blogs.blog_url from rss, users, blogs where rss.blog_id = blogs.blog_id and rss.user_id = users.user_id order by rss.date_parsed desc limit $offset,$page_size");
+
+$entries = $db->get_results("select rss.blog_id, rss.user_id, title, url, user_login, user_avatar, blogs.blog_url, blogs.blog_title from rss, users, blogs where rss.blog_id = blogs.blog_id and rss.user_id = users.user_id order by rss.date_parsed desc limit $offset,$page_size");
 
 if ($entries) {
-	echo '<div style="margin-left:20px">';
 	foreach ($entries as $entry) {
-		echo '<h4>';
-		echo '<a href="'.get_user_uri($entry->user_login).'" class="tooltip u:'.$entry->user_id.'"><img class="avatar" src="'.get_avatar_url($entry->user_id, $entry->user_avatar, 25).'" width="25" height="25" alt="avatar"/></a>&nbsp;';
-		echo "<a href='$entry->url' rel='nofollow'>$entry->title</a></h4>";
+		$title = strip_tags($entry->title);
+		$url = clean_input_string($entry->url);
+		$blog_title = strip_tags($entry->blog_title);
+
+		echo '<tr>';
+		echo '<td style="width:45px"><a href="'.get_user_uri($entry->user_login).'" class="tooltip u:'.$entry->user_id.'"><img class="avatar" src="'.get_avatar_url($entry->user_id, $entry->user_avatar, 40).'" width="40" height="40" alt="avatar"/></a></td>';
+		echo '<td style="font-size:110%;width:30%"><a href="'.$entry->blog_url.'" rel="nofollow">'.$blog_title.'</a></td>';
+		echo '<td style="font-size:120%"><a href="'.$url.'" rel="nofollow">'.$title.'</a></td>';
+		echo '</tr>';
 	}
-	echo '</div>';
 
 }
-
+echo '</table>';
 
 do_pages($rows, $page_size);
 echo '</div>';
