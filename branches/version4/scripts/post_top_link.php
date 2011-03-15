c84c08cbe2db5fbe5c0d3797f0f60dc0b7dfe62f
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-03-15 15:41:47 +0000

    Script to post "top story" ("destacada") to Twitter and Jaiku
    git-svn-id: file:///home/gallir/meneame/svn/src@2917 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version4/scripts/post_top_link.php
@@ -0,0 +1,49 @@
+<?
+// Post to Twitter/Jaiku the "top story"
+
+include('../config.php');
+include(mnminclude.'external_post.php');
+
+
+$a_tops = new Annotation('top-links');
+if(!$a_tops->read()) {
+	exit;
+}
+$tops = explode(',', $a_tops->text);
+
+$a_history = new Annotation('top-links-post-history');
+if($a_history->read()) {
+	$history = explode(',',$a_history->text);
+} else {
+	$history = array();
+}
+
+if (! in_array($tops[0], $history) ) {
+	if( ! ($link = Link::from_db($tops[0])) ) {
+		echo "Error reading link ". $tops[0] . "\n";
+		exit;
+	}
+	if ($globals['url_shortener']) {
+		$short_url = $link->get_short_permalink();
+	} else {
+		$short_url = fon_gs($link->get_permalink());
+	}
+	$text = '#'._('destacada').' '.$link->title;
+
+	// Save the history
+	array_push($history, intval($tops[0]));
+	while(count($history) > 10) array_shift($history);
+	$a_history->text = implode(',',$history);
+	$a_history->store();
+
+
+	// Post to Twitter and Jaiku
+	if ($globals['twitter_token']) {
+		twitter_post($text, $short_url); 
+	}
+	if ($globals['jaiku_user'] && $globals['jaiku_key']) {
+		jaiku_post($text, $short_url); 
+	}
+}
+
+?>
