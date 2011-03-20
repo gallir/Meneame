16d238f159c87bb8a30da975ce60efab19d7b43b
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-03-20 18:22:06 +0000

    Store the top ten
    git-svn-id: file:///home/gallir/meneame/svn/src@2931 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/scripts/top-news.py
+++ branches/version4/scripts/top-news.py
@@ -13,7 +13,7 @@ from utils import *
 def main():
 	links = {}
 	cursor = DBM.cursor()
-	cursor.execute("select link_id, link_uri, unix_timestamp(now()) - unix_timestamp(link_date) from links where link_status = 'published' and link_date > date_sub(now(), interval 24 hour) and link_votes > link_negatives/20 order by link_date desc")
+	cursor.execute("select link_id, link_uri, unix_timestamp(now()) - unix_timestamp(link_date) from links where link_status = 'published' and link_date > date_sub(now(), interval 24 hour) and link_votes/20 > link_negatives order by link_date desc")
 	links_total = 0
 	for row in cursor:
 		links_total += 1
@@ -29,7 +29,7 @@ def main():
 
 	links_format = ','.join(['%s'] * len(links))
 
-	cursor.execute("select vote_link_id, sum((1-(unix_timestamp(now())-unix_timestamp(vote_date))/28800)) as x, count(*) from votes where vote_link_id in (%s) and vote_type='links' and vote_date > date_sub(now(), interval 8 hour) and vote_user_id > 0 and vote_value > 7 group by vote_link_id order by x desc" % links_format, tuple(links))
+	cursor.execute("select vote_link_id, sum((1-(unix_timestamp(now())-unix_timestamp(vote_date))/28800)) as x, count(*) from votes where vote_link_id in (%s) and vote_type='links' and vote_date > date_sub(now(), interval 8 hour) and vote_user_id > 0 and vote_value > 6.1 group by vote_link_id order by x desc" % links_format, tuple(links))
 	votes_total = 0;
 	votes_links = 0
 	v_total = 0
@@ -44,7 +44,7 @@ def main():
 	votes_average = votes_total/votes_links
 
 		
-	cursor.execute("select comment_link_id, sum(2*(1-(unix_timestamp(now())-unix_timestamp(comment_date))/28800)), count(*)  from comments where comment_link_id in (%s) and comment_date > date_sub(now(), interval 8 hour) group by comment_link_id" % links_format, tuple(links))
+	cursor.execute("select comment_link_id, sum(1.5*(1-(unix_timestamp(now())-unix_timestamp(comment_date))/28800)), count(*)  from comments where comment_link_id in (%s) and comment_date > date_sub(now(), interval 8 hour) group by comment_link_id" % links_format, tuple(links))
 	comments_total = 0
 	comments_links = 0
 	c_total = 0
@@ -66,21 +66,29 @@ def main():
 	print "Votes average:", votes_average, v_average, "Comments average:", comments_average, c_average
 	for id in links:
 		if links[id]['c'] > 0 and links[id]['v'] > 0:
-			links[id]['w'] = (1 - links[id]['old']/(2*86400)) * (links[id]['v'] + links[id]['c'] + links[id]['clicks'] * (1 - links[id]['old']/86400) * 0.005)
+			links[id]['w'] = (1 - links[id]['old']/(1.5*86400)) * (links[id]['v'] + links[id]['c'] + links[id]['clicks'] * (1 - links[id]['old']/86400) * 0.005)
 
 	sorted_ids = sorted(links, cmp=lambda x,y: cmp(links[y]['w'], links[x]['w']))
+
+	if sorted_ids:
+		str = ','.join([unicode(x) for x in sorted_ids[:10]])
+		c = DBM.cursor('update')
+		c.execute("replace into annotations (annotation_key, annotation_expire, annotation_text) values('top-active', date_add(now(), interval 15 minute), %s)", (str,))
+		c.close()
+		DBM.commit()
+
 	i = 0
 	for id in sorted_ids:
 		if links[id]['w'] > 0 and i < 10:
 			i += 1
-			print i, links[id]['links_order'], links[id]['old'], links[id]['uri'], links[id]['w'], "votes:", links[id]['votes'], links[id]['votes_order'], links[id]['v'], "comments:", links[id]['comments'], links[id]['c'], "clicks:", links[id]['clicks'], links[id]['clicks'] * (1 - links[id]['old']/86400) * 0.005
+			print i, links[id]['links_order'], links[id]['old'], id, links[id]['uri'], links[id]['w'], "votes:", links[id]['votes'], links[id]['votes_order'], links[id]['v'], "comments:", links[id]['comments'], links[id]['c'], "clicks:", links[id]['clicks'], links[id]['clicks'] * (1 - links[id]['old']/86400) * 0.004
 
 	# Select the top stories
 	str = ','.join([unicode(x) for x in sorted_ids if links[x]['w'] > dbconf.tops['min-weight'] and (links[x]['links_order'] > 2 or links[x]['old'] > 3600) and links[x]['c'] > c_average * 3.5 and links[x]['v'] > v_average * 3.5 and links[x]['votes_order'] <= 10 ])
 
 	if str:
 		c = DBM.cursor('update')
-		c.execute("replace into annotations (annotation_key, annotation_expire, annotation_text) values('top-links', date_add(now(), interval 15 minute), %s)", (str,))
+		c.execute("replace into annotations (annotation_key, annotation_expire, annotation_text) values('top-links', date_add(now(), interval 10 minute), %s)", (str,))
 		c.close()
 		DBM.commit()
 		print "Stored:", str
