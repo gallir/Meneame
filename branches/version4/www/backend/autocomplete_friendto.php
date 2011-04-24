1399379a10139d76dc6f8cae4d95467f1248ea34
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-04-24 22:18:25 +0000

    Autocomplete missing
    git-svn-id: file:///home/gallir/meneame/svn/src@2972 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version4/www/backend/autocomplete_friendto.php
@@ -0,0 +1,33 @@
+<?
+// The source code packaged with this file is Free Software, Copyright (C) 2005 by
+// Ricardo Galli <gallir at uib dot es>.
+// It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
+// You can get copies of the licenses here:
+// 		http://www.affero.org/oagpl.html
+// AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
+
+include('../config.php');
+
+if (! $current_user->user_id) {
+	do_error(_('debe autentificarse'), 403);
+}
+
+header('Content-Type: text/plain; charset=UTF-8');
+
+$q = '';
+if (isset($_GET['q'])) {
+    $q = mb_strtolower($_GET['q']);
+}
+if (!$q) {
+    return;
+}
+
+$users = $db->get_col("select user_login from users, friends where friend_type = 'manual' and friend_to = $current_user->user_id and friend_value > 0 and user_id = friend_from and user_login like '$q%'");
+
+if ($users) {
+	foreach ($users as $user) {
+		echo mb_strtolower($user)."\n";
+	}
+}
+
+?>
