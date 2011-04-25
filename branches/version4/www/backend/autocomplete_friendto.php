10d9f86d5de11c868653187f54fd478c35b7e432
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-04-25 10:38:38 +0000

    trim and escape input string.
    git-svn-id: file:///home/gallir/meneame/svn/src@2976 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/backend/autocomplete_friendto.php
+++ branches/version4/www/backend/autocomplete_friendto.php
@@ -16,12 +16,13 @@ header('Content-Type: text/plain; charset=UTF-8');
 
 $q = '';
 if (isset($_GET['q'])) {
-    $q = mb_strtolower($_GET['q']);
+    $q = mb_strtolower(trim($_GET['q']));
 }
 if (!$q) {
     return;
 }
 
+$q = $db->escape($q);
 $users = $db->get_col("select user_login from users, friends where friend_type = 'manual' and friend_to = $current_user->user_id and friend_value > 0 and user_id = friend_from and user_login like '$q%'");
 
 if ($users) {
