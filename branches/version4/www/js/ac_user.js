74daec1a37159856d0b409f5e16f78fe9b3704d1
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-07-06 19:35:26 +0000

    Solved the conflict with several input and avatars in the same form
    git-svn-id: file:///home/gallir/meneame/svn/src@3056 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/js/ac_user.js
+++ branches/version4/www/js/ac_user.js
@@ -1,36 +1,38 @@
-var ac_selected_url = false;
 var ac_extraParams = { avatar: 1 };
 
 if (typeof ac_minChars == "undefined") ac_minChars = 3;
 if (typeof ac_friends != "undefined" && ac_friends) ac_extraParams.friends=1;
 
-$(".ac_user").attr("autocomplete", "off").autocomplete({ url:base_url+'backend/autocomplete_user.php',
-	minChars: ac_minChars, matchSubset: 0,
-	extraParams: ac_extraParams,
-	showResult: function(value, data) {
-		return '<img src="'+data[0]+'" width="12" height="12" style="vertical-align:bottom"/>&nbsp;'+value;
-	},
-	onItemSelect: function(item) {
-		if (name.length > 0 && item.data.length > 0 && item.data[0].length > 0) {
-			ac_selected_url = item.data[0];
-			id = $(this).attr("id");
-			$("#"+id+"_avatar").attr("src", ac_selected_url);
-			$("#"+id+"_avatar").css('visibility', 'visible');
-		} else {
-			ac_selected_url = false;
+$(".ac_user").attr("autocomplete", "off").each( function () {
+	var input = $(this);
+	input.data("url", false);
+	$(this).autocomplete({ url:base_url+'backend/autocomplete_user.php',
+		minChars: ac_minChars, matchSubset: 0,
+		extraParams: ac_extraParams,
+		showResult: function(value, data) {
+			return '<img src="'+data[0]+'" width="12" height="12" style="vertical-align:bottom"/>&nbsp;'+value;
+		},
+		onItemSelect: function(item) {
+			if (name.length > 0 && item.data.length > 0 && item.data[0].length > 0) {
+				input.data("url", item.data[0]).change();
+			} else {
+				input.data("url", false);
+			}
 		}
-	},
-}).change(function (t) {id = $(this).attr("id"); setTimeout(function () {form_add_avatar(id)}, 300) });
+	}).change(function () {setTimeout(function() {form_add_avatar(input)}, 100)});
+});
 
-function form_add_avatar(id) {
-	name = $("#"+id).val();
+function form_add_avatar(input) {
+	name = input.val();
+	avatar_holder = $("#"+input.attr('id')+"_avatar");
 	if (name.length > 0) {
-		if (ac_selected_url != false) url = ac_selected_url;
+		if (input.data('url') != false) url = input.data('url');
 		else url = base_url+"backend/get_avatar.php?user="+name+"&size=20"
-		$("#"+id+"_avatar").attr("src", url);
-		$("#"+id+"_avatar").css('visibility', 'visible');
+		if (avatar_holder.attr("src") != url) {
+			avatar_holder.attr("src", url).css('visibility', 'visible');
+		}
 	} else {
-		$("#"+id+"_avatar").css('visibility', 'hidden');
+		avatar_holder.css('visibility', 'hidden').attr("src", "");
 	}
-	ac_selected_url = false;
+	input.data("url", false);
 }
