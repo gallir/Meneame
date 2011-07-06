0545f9d7fb5ccb31fa8dc13b9c51bd95a7149008
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-07-06 00:20:48 +0000

    Cosmetic changes.
    git-svn-id: file:///home/gallir/meneame/svn/src@3054 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/config.php
+++ branches/version4/www/config.php
@@ -285,7 +285,7 @@ $globals['Amazon_S3_delete_allowed'] = false;
 // Main javascript file
 $globals['js_main'] = 'general.js.php?v=36'; //?N is to force reloading for newer versions
 // CSS files (main, color, notame)
-$globals['css_main'] = 'css/es/mnm.css?v=35'; //?N is to force reloading for newer versions
+$globals['css_main'] = 'css/es/mnm.css?v=37'; //?N is to force reloading for newer versions
 //$globals['css_color'] = 'css/es/mnmcol17.css';
 $globals['html_main'] = 'html1.php';
 
