74e1b7aa7169b5d6697ef8ece7410850a72af3b4
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-09-20 19:02:11 +0000

    Give bigger weight to larger comments
    git-svn-id: file:///home/gallir/meneame/svn/src@3144 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/story.php
+++ branches/version4/www/story.php
@@ -480,7 +480,7 @@ function print_relevant_comments($link) {
 
 	if($do_cache) {
 		$key = 'relevant_story_comments_'.$globals['css_main'].$link->id;
-		if(memcache_mprint($key)) return;
+		//if(memcache_mprint($key)) return;
 	}
 
 
@@ -491,8 +491,9 @@ function print_relevant_comments($link) {
 	$extra_limit = $limit * 2;
 	$min_len = 32;
 	$min_karma = max(20, $karma/2);
+	$check_vote = $link->date - ($globals['now'] - $globals['time_enabled_votes']);
 
-	$res = $db->get_results("select comment_id, comment_order, comment_karma + comment_order * 0.7 as val, length(comment_content) as comment_len, user_id, user_avatar, vote_value from comments LEFT JOIN votes ON (vote_type = 'links' and vote_link_id = comment_link_id and vote_user_id = comment_user_id), users where comment_link_id = $link->id and comment_karma > $min_karma and length(comment_content) > $min_len and comment_user_id = user_id order by val desc limit $extra_limit");
+	$res = $db->get_results("select comment_id, comment_order, comment_karma + comment_order * 0.7 as val, length(comment_content) as comment_len, user_id, user_avatar, vote_value from comments LEFT JOIN votes ON ($check_vote > 0 and vote_type = 'links' and vote_link_id = comment_link_id and vote_user_id = comment_user_id), users where comment_link_id = $link->id and comment_karma > $min_karma and length(comment_content) > $min_len and comment_user_id = user_id order by val desc limit $extra_limit");
 
 	function cmp_comment_val($a, $b) {
 		if ($a->val == $b->val) return 0;
@@ -507,6 +508,8 @@ function print_relevant_comments($link) {
 			if ($comment->vote_value < 0 && $comment->comment_len > 60) {
 				$comment->val *= 2;
 			}
+			// Gives a little advantage to larger comments
+			$comment->val *=  min(1.5, log($comment->comment_len, 10) / 1.8);
 		}
 		usort($res, "cmp_comment_val");
 		foreach ($res as $comment) {
