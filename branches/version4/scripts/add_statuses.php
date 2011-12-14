cee6255db1483725916a0dfcb082c876bfe43d80
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2011-12-14 05:57:16 +0000

    Improved with transactions
    git-svn-id: file:///home/gallir/meneame/svn/src@3242 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/scripts/add_statuses.php
+++ branches/version4/scripts/add_statuses.php
@@ -4,11 +4,19 @@
 
 include('../config.php');
 
-$links = $db->object_iterator("SELECT  link_id as id, link_status as status, link_karma as karma, link_category as category,  UNIX_TIMESTAMP(link_date) as date,  UNIX_TIMESTAMP(link_sent_date) as sent_date, UNIX_TIMESTAMP(link_published_date) as published_date FROM links order by link_id asc", "Link");
+$links = $db->object_iterator("SELECT  link_id as id, link_status as status, link_karma as karma, link_category as category,  UNIX_TIMESTAMP(link_date) as date,  UNIX_TIMESTAMP(link_sent_date) as sent_date, UNIX_TIMESTAMP(link_published_date) as published_date FROM links order by link_id desc", "Link");
 if ($links) {
+	$c = 0;
+	$db->transaction();
 	foreach($links as $link) {
-		echo "$link->id, $link->category\n";
+		if ($c % 1000 == 0) {
+			echo "$link->id, $link->category\n";
+			$db->commit();
+			usleep(100000);
+			$db->transaction();
+		}
 		SitesMgr::deploy($link);
-		usleep(10000);
+		$c++;
 	}
+	$db->commit();
 }
