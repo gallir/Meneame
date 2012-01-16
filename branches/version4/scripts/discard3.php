dbbfb08ba0bc0574b234322fb1dd8b5c44acab50
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2012-01-16 13:02:11 +0000

    Fixed bug, date comparison was wrong, $l->date was in unix_timestamp, not in mysql native format
    git-svn-id: file:///home/gallir/meneame/svn/src@3320 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/scripts/discard3.php
+++ branches/version4/scripts/discard3.php
@@ -126,8 +126,8 @@ function depublish($site_id) {
 		foreach ($links as $link) {
 			$l = Link::from_db($link);
 			// Count only those votes with karma > 6 to avoid abuses with new accounts with new accounts
-			$negatives = (int) $db->get_var("select SQL_NO_CACHE sum(user_karma) from votes, users where vote_type='links' and vote_link_id=$l->id and vote_date > '$l->date' and vote_date > date_sub(now(), interval 24 hour) and vote_value < 0 and vote_user_id > 0 and user_id = vote_user_id and user_karma > " . $globals['depublish_negative_karma']);
-			$positives = (int) $db->get_var("select SQL_NO_CACHE sum(user_karma) from votes, users where vote_type='links' and vote_link_id=$l->id and vote_date > '$l->date' and vote_value > 0 and vote_date > date_sub(now(), interval 24 hour) and vote_user_id > 0 and user_id = vote_user_id and user_karma > " . $globals['depublish_positive_karma']);
+			$negatives = (int) $db->get_var("select SQL_NO_CACHE sum(user_karma) from votes, users where vote_type='links' and vote_link_id=$l->id and vote_date > from_unixtime($l->date) and vote_date > date_sub(now(), interval 24 hour) and vote_value < 0 and vote_user_id > 0 and user_id = vote_user_id and user_karma > " . $globals['depublish_negative_karma']);
+			$positives = (int) $db->get_var("select SQL_NO_CACHE sum(user_karma) from votes, users where vote_type='links' and vote_link_id=$l->id and vote_date > from_unixtime($l->date) and vote_value > 0 and vote_date > date_sub(now(), interval 24 hour) and vote_user_id > 0 and user_id = vote_user_id and user_karma > " . $globals['depublish_positive_karma']);
 			echo "Candidate $l->id ($l->karma) $negatives $positives\n";
 			if ($negatives > $l->karma/6 && $l->negatives > $l->votes/6
 				&& ($negatives > $positives || ($negatives > $l->karma/2 && $negatives > $positives/2) )) {
