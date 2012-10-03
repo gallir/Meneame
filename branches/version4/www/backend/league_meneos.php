f4d3e241b940d13019e652e0f933a3c0285e69a6
benjami <benjami@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2012-10-03 21:45:20 +0000

    Very very minor change
    git-svn-id: file:///home/gallir/meneame/svn/src@3509 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/backend/league_meneos.php
+++ branches/version4/www/backend/league_meneos.php
@@ -66,7 +66,7 @@ if (!empty($votes[0])) {
 
 foreach ( $votes as $vote) {
 	echo '<div class="item' . ($vote->value == $win_class ? ' winner' : '') . '">';
-	$vote_detail = _('votó')."&nbsp;" . $globals['vote_values'][$vote->value];
+	$vote_detail = _('votó al')."&nbsp;" . $globals['vote_values'][$vote->value];
 	$vote_detail .= ' '._('el').date(" d-m-Y, H:i:s",strtotime($vote->vdate));
 	echo '<a href="'.get_user_uri($vote->user_login).'" title="'.$vote_detail.'">';
 	echo '<img class="avatar" src="'.get_avatar_url($vote->user_id, $vote->user_avatar, 20).'" width="20" height="20" alt=""/>';
