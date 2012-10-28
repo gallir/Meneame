3121ae658480d34208e147507024c56da32088b4
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2012-10-28 16:07:48 +0000

    New boxes, step 1.
    git-svn-id: file:///home/gallir/meneame/svn/src@3532 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/config.php
+++ branches/version4/www/config.php
@@ -39,7 +39,7 @@ $globals['ssl_server'] = False;
 
 $globals['site_name'] = 'Menéame';
 $globals['site_shortname'] = 'mnm'; //Used to differentiate in keys
-$globals['v'] = 7; // internal version, to for reloads
+$globals['v'] = 8; // internal version, to for reloads
 $globals['lang'] = $dblang	= 'es';
 
 $globals['help_url'] = 'http://meneame.wikispaces.com/Comenzando';
