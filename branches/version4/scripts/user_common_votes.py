0e7a61596e2d1988e8caef3adcf2dcc6e6661f97
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2012-11-03 22:48:50 +0000

    Ignore one simmilarity if it's a queued link. It ensures the "base density" is 0 for queued links
    git-svn-id: file:///home/gallir/meneame/svn/src@3539 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/scripts/user_common_votes.py
+++ branches/version4/scripts/user_common_votes.py
@@ -15,7 +15,10 @@ from utils import *
 
 def main():
 	links = {}
+	status = {}
 	commons = {}
+	queued_avoided = set()
+	ages = {}
 
 	parser = argparse.ArgumentParser(description='Store the number of common votes among users')
 	parser.add_argument("hours", help="Hours to analyze", type=int)	
@@ -31,7 +34,7 @@ def main():
 
 
 	cursor = DBM.cursor()
-	cursor.execute("select link_id, link_date, link_status, vote_user_id, vote_date, vote_value FROM votes, links WHERE vote_type = 'links' and vote_user_id > 0 and vote_date > date_sub(now(), interval %s hour) and vote_date < date_sub(now(), interval %s hour) and link_id = vote_link_id" % (time_from, time_to))
+	cursor.execute("select link_id, link_date, link_status, vote_user_id, vote_date, vote_value, UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP(link_date)  FROM votes, links WHERE vote_type = 'links' and vote_user_id > 0 and vote_date > date_sub(now(), interval %s hour) and vote_date < date_sub(now(), interval %s hour) and link_id = vote_link_id" % (time_from, time_to))
 
 	for row in cursor:
 		if row[4] > now:
@@ -41,20 +44,33 @@ def main():
 		
 		if row[0] not in links:
 			links[row[0]] = set()
+			if row[0] not in status:
+				status[row[0]] = row[2]
+				ages[row[0]] = row[6]
 
 		e = int(row[3] * (row[5] / abs(row[5])))
 		links[row[0]].add(e)
 
 
-	for l in links:
+	sorted_links = sorted(links, reverse=True)
+
+	for l in sorted_links:
 		for x in links[l]:
 			for y in links[l]:
 				ax = abs(x)
 				ay = abs(y)
 				if ax < ay:
+
+					if status[l] != "published" and ages[l] < 3600*12:
+						""" Avoid counting one vote of new queued links """
+						key = "%d:%d" % (ax, ay)
+						if key not in queued_avoided:
+							queued_avoided.add(key)
+							continue
+
 					if ax not in commons: commons[ax] = {}
 					if ay not in commons[ax]: commons[ax][ay] = 0
-					if x*y > 0: 
+					if x*y > 0:
 						commons[ax][ay] += 1
 					else:
 						commons[ax][ay] -= 1
@@ -63,13 +79,15 @@ def main():
 	c.execute("delete from users_similarities where date < date_sub(now(), interval 45 day)")
 	DBM.commit()
 
-
 	alpha = 0.9**(int(sys.argv[1])/24)
 	print "Alpha: ", alpha, "Rows: ", len(commons)
 	ops = 0
 	#now = str(now)
 	for k in sorted(commons):
 		for l in sorted(commons[k]):
+
+			if commons[k][l] == 0: continue
+
 			ops += 1
 			sql = "INSERT INTO users_similarities (minor, major, value, date) VALUES (%s, %s, %s, '%s') ON DUPLICATE KEY UPDATE value = GREATEST(%s, %s * value * (1 - LEAST(1, timestampdiff(day, date, '%s')/60) ) + (1-%s) * %s), date = '%s'" % (k, l, commons[k][l], now, commons[k][l], alpha, now, alpha, commons[k][l], now)
 			#print sql
