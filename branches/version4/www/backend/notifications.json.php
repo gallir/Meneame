5f016865655f8fd9564598fbc5ac5dadfec3945b
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2013-03-17 22:25:22 +0000

    Added cache to notifications
    git-svn-id: file:///home/gallir/meneame/svn/src@3589 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/backend/notifications.json.php
+++ branches/version4/www/backend/notifications.json.php
@@ -11,13 +11,27 @@ $globals['alternate_db_server'] = 'backend';
 
 include('../config.php');
 
-include(mnminclude.'ban.php');
-check_ip_noaccess();
+if (! $current_user->user_id) die;
 
 header('Content-Type: application/json; charset=utf-8');
 http_cache(5);
 
-if (! $current_user->user_id) die;
+if (isset($_GET['totals'])) {
+	$do_totals = true;
+} else {
+	$do_totals = false;
+}
+
+$cache_key = 'notifications_'.$current_user->user_id.'_'.$do_totals;
+if(memcache_mprint($cache_key)) {
+	echo "\n/*CACHED $cache_key*/";
+    exit(0);
+}
+
+include(mnminclude.'ban.php');
+check_ip_noaccess();
+
+
 
 $notifications = new stdClass();
 
@@ -26,49 +40,51 @@ $notifications->comments = Comment::get_unread_conversations($current_user->user
 $notifications->privates = PrivateMessage::get_unread($current_user->user_id);
 $notifications->friends = count(User::get_new_friends($current_user->user_id));
 
-if (isset($_GET['totals'])) {
+if ($do_totals) {
 	$notifications->total = $notifications->posts + $notifications->privates + $notifications->friends + $notifications->comments;
-	echo json_encode($notifications);
-	exit(0);
+	$response = json_encode($notifications);
+} else {
+	$objects = array();
+
+	$obj = new stdClass();
+	$obj->count = $notifications->privates;
+	$obj->text = _('privados nuevos');
+	$obj->icon = $globals['base_static'].'img/common/icon_message-02.png';
+	$obj->width=19;
+	$obj->height=19;
+	$obj->url = post_get_base_url('_priv');
+	$objects[] = $obj;
+
+	$obj = new stdClass();
+	$obj->count = $notifications->posts;
+	$obj->text = _('respuestas a notas');
+	$obj->icon = $globals['base_static'].'img/common/icon_post-02.png';
+	$obj->width=19;
+	$obj->height=19;
+	$obj->url = post_get_base_url($current_user->user_login) . '/_conversation';
+	$objects[] = $obj;
+
+	$obj = new stdClass();
+	$obj->count = $notifications->comments;
+	$obj->text = _('respuestas a comentarios');
+	$obj->icon = $globals['base_static'].'img/common/icon_comment-01.png';
+	$obj->width=19;
+	$obj->height=19;
+	$obj->url = get_user_uri($current_user->user_login, 'conversation');
+	$objects[] = $obj;
+
+	$obj = new stdClass();
+	$obj->count = $notifications->friends;
+	$obj->text = _('nuevos amigos');
+	$obj->icon = $globals['base_static'].'img/common/icon_friend_bi_00.png';
+	$obj->width=18;
+	$obj->height=16;
+	$obj->url = get_user_uri($current_user->user_login, 'friends_new');
+	$objects[] = $obj;
+
+	$response = json_encode($objects);
 }
 
-$objects = array();
-
-$obj = new stdClass();
-$obj->count = $notifications->privates;
-$obj->text = _('privados nuevos');
-$obj->icon = $globals['base_static'].'img/common/icon_message-02.png';
-$obj->width=19;
-$obj->height=19;
-$obj->url = post_get_base_url('_priv');
-$objects[] = $obj;
-
-$obj = new stdClass();
-$obj->count = $notifications->posts;
-$obj->text = _('respuestas a notas');
-$obj->icon = $globals['base_static'].'img/common/icon_post-02.png';
-$obj->width=19;
-$obj->height=19;
-$obj->url = post_get_base_url($current_user->user_login) . '/_conversation';
-$objects[] = $obj;
-
-$obj = new stdClass();
-$obj->count = $notifications->comments;
-$obj->text = _('respuestas a comentarios');
-$obj->icon = $globals['base_static'].'img/common/icon_comment-01.png';
-$obj->width=19;
-$obj->height=19;
-$obj->url = get_user_uri($current_user->user_login, 'conversation');
-$objects[] = $obj;
-
-$obj = new stdClass();
-$obj->count = $notifications->friends;
-$obj->text = _('nuevos amigos');
-$obj->icon = $globals['base_static'].'img/common/icon_friend_bi_00.png';
-$obj->width=18;
-$obj->height=16;
-$obj->url = get_user_uri($current_user->user_login, 'friends_new');
-$objects[] = $obj;
-
-echo json_encode($objects);
+memcache_madd($cache_key, $response, 10);
+echo $response;
 
