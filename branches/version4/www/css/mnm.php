bf7246476c1d74945715972edc0532c2a0a669b2
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2013-11-14 15:14:26 +0000

    Add Last-Modified header
    git-svn-id: file:///home/gallir/meneame/svn/src@3940 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/css/mnm.php
+++ branches/version4/www/css/mnm.php
@@ -3,6 +3,7 @@ include('../config.php');
 header('Content-Type: text/css; charset=utf-8');
 header('Cache-Control: public, max-age=864000');
 header("Expires: " . gmdate("r", $globals['now'] + 864000));
+header('Last-Modified: ' .  gmdate('D, d M Y H:i:s', filemtime('mnm.css')) . ' GMT');
 
 Haanga::Load('css/colorbox.css');
 Haanga::Load('css/mnm.css');
--- branches/version4/www/js/main.js.php
+++ branches/version4/www/js/main.js.php
@@ -3,6 +3,7 @@ include('../config.php');
 header('Content-Type: application/x-javascript; charset=utf-8');
 header('Cache-Control: public, max-age=864000');
 header("Expires: " . gmdate("r", $globals['now'] + 864000));
+header('Last-Modified: ' .  gmdate('D, d M Y H:i:s', filemtime('main.js')) . ' GMT');
 
 Haanga::Load('js/main.js');
 Haanga::Load('js/jquery.colorbox-min.js');
