e03a960c9561f9e34e809ab572ae844a91c22d85
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2013-12-01 11:44:21 +0000

    Use jquery 2 in newer browsers
    git-svn-id: file:///home/gallir/meneame/svn/src@3958 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/config.php
+++ branches/version4/www/config.php
@@ -369,6 +369,7 @@ $globals['Amazon_S3_delete_allowed'] = false;
 
 // Main javascript file
 $globals['jquery'] = '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
+$globals['jquery2'] = '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js';
 $globals['js_main'] = 'main.js.php';
 // CSS files (main, color, notame)
 $globals['css_main'] = 'mnm.php';
