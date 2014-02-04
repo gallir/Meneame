2acad92667e84e078c5f002c9f3a061599f337d5
gallir <gallir@cbe1a1ec-7179-4919-8e32-4551e8279a00>
2014-02-04 13:13:51 +0000

    Security check to avoid private fields requests. Added requests for comments and posts.
    git-svn-id: file:///home/gallir/meneame/svn/src@4029 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version4/www/backend/info.php
+++ branches/version4/www/backend/info.php
@@ -1,10 +1,13 @@
 <?
 include_once('../config.php');
+$forbidden = array('ip', 'email', 'ip_int', 'user_level');
+
+
 header('Content-Type: application/json; charset=utf-8');
 
 if(empty($_GET['id']) || empty($_GET['fields'])) die;
 $id = intval($_GET['id']);
-$fields = clean_input_string($_GET['fields']);
+$fields = clean_input_string($_GET['fields']); // It has to remove parenthesis
 
 if (empty($_GET['what'])) $what = 'link';
 else $what = $_GET['what'];
@@ -12,7 +15,16 @@ else $what = $_GET['what'];
 $object = false;
 switch ($what) {
 	case 'link':
-		$object = Link::from_db($id);
+	case 'links':
+		$object = Link::from_db($id, null, false);
+		break;
+	case 'comment':
+	case 'comments':
+		$object = Comment::from_db($id);
+		break;
+	case 'post':
+	case 'posts':
+		$object = Post::from_db($id);
 		break;
 }
 
@@ -20,7 +32,9 @@ if(!$object) die;
 
 $output = new stdClass();
 foreach (preg_split('/,/', $fields, 10, PREG_SPLIT_NO_EMPTY) as $f) {
-	$output->$f = $object->$f;
+	if (! in_array($f, $forbidden) && property_exists($object, $f)) {
+		$output->$f = $object->$f;
+	}
 }
 
 echo json_encode($output);
