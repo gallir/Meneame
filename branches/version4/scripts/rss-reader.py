fddb262b45d7f6ce52195509a5e4556895f400b4
Espartaco Palma <esparta@gmail.com>
2014-03-03 21:07:36 -0600

    Removing unused variables on rss-reader.py. Issue #3 users_ids and blogs_ids seems like not used. Perhaps they were used as testing values while @gallir was scripting this.

--- branches/version4/scripts/rss-reader.py
+++ branches/version4/scripts/rss-reader.py
@@ -96,8 +96,6 @@ def get_candidate_blogs(days, min_karma):
 	now = time.time()
 	blogs = set()
 	results = set()
-	blogs_ids = set()
-	users_ids = set()
 	cursor = DBM.cursor()
 	inner_cursor = DBM.cursor()
 
@@ -149,8 +147,7 @@ def get_candidate_blogs(days, min_karma):
 			if result is not None:
 				blog.user, blog.user_id, blog.karma = result
 				blogs.add(blog)
-				blogs_ids.add(blog.id)
-				users_ids.add(blog.user_id)
+
 
 	# Select active users that have no published posts
 	query = """
@@ -180,10 +177,8 @@ def get_candidate_blogs(days, min_karma):
 		blog.checked, blog.read, blog.user, blog.user_id, blog.karma = row
 		blog.base_url = blog.url.replace('http://', '').\
 							replace('https://', '').replace('www.', '')
-		if blog.id not in blogs_ids and blog.user_id not in users_ids:
-				blogs.add(blog)
-				users_ids.add(blog.user_id)
-				blogs_ids.add(blog.id)
+		blogs.add(blog)
+
 
 	feeds_read = 0
 	# Sort the set of blogs by date of read
