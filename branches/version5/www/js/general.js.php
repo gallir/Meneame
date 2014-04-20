d23d48b278d58b1e8e5c98f049d0dc352bed64a0
Ricardo Galli <gallir@gmail.com>
2014-04-20 17:06:36 +0000

    Removed, old and not needed.
    git-svn-id: file:///home/gallir/meneame/svn/src@4226 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version5/www/js/general.js.php
+++
@@ -1,8 +0,0 @@
-<?
-include('../config.php');
-header('Content-Type: application/x-javascript; charset=utf-8');
-header('Cache-Control: public, max-age=864000');
-header("Expires: " . gmdate("r", $globals['now'] + 864000));
-
-Haanga::Load('js/general.js');
-?>
