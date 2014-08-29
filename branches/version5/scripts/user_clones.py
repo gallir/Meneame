407e517272ead47cd6ea7325707d9b07eb6ac6ef
Ricardo Galli <gallir@gmail.com>
2014-08-29 16:55:50 +0000

    Python version (and improved) to user_clones.pl
    git-svn-id: file:///home/gallir/meneame/svn/src@4327 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version5/scripts/user_clones.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import dbconf
+from utils import DBM
+import argparse
+
+def main():
+	global configuration
+	user = configuration.user
+
+	cursor = DBM.cursor()
+
+	query = """select distinct clon.user_login, clon.user_level, clon_ip, clon_date from users, users as clon, clones where users.user_login = %s and clon_from = users.user_id and clon_to = clon.user_id and clon_date > date_sub(now(), interval 60 day)"""
+
+	cursor.execute(query, (user,))
+	for clon, level, ip, date in cursor:
+		print("%-16s\t%s\t%s\t%s" % (clon, ip, level, date))
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("user", help="Shows the users' clones")
+	configuration = parser.parse_args()
+	main()
+
+
+
