1663f611224a24d7c94b0206797938e4d88d7fa3
Ricardo Galli <gallir@gmail.com>
2014-09-21 02:43:54 +0000

    Silent the redirectionSilent the redirection..
    git-svn-id: file:///home/gallir/meneame/svn/src@4353 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version5/www/submnm.php
+++ branches/version5/www/submnm.php
@@ -4,21 +4,21 @@ $path = $globals['path'];
 $globals['submnm'] = preg_replace('/[^\p{L}\d_]/u', ':', $path[1]);
 include_once 'config.php';
 
-$globals['site_shortname'] = $globals['submnm'];
-if (empty($globals['submnm']) || ! ($info = SitesMgr::get_info())) {
-	not_found();
-}
-
 $forbidden_routes = array('m', 'user', 'legal', 'notame', 'mobile', 'register', 'login', 'trends');
 
 if (in_array($path[2], $forbidden_routes)) {
-	syslog(LOG_INFO, "Forbidden in subs: ".$path[2]);
+	// syslog(LOG_INFO, "Forbidden in subs: ".$path[2]);
 	// Redirect to the root
 	$uri = preg_split('/\/+/', $_SERVER['REQUEST_URI'], 10, PREG_SPLIT_NO_EMPTY);
 	$uri = array_slice($uri, 2);
 	$uri = '/'.implode('/', $uri);
 	header("Location: $uri");
 	die;
+}
+
+$globals['site_shortname'] = $globals['submnm'];
+if (empty($globals['submnm']) || ! ($info = SitesMgr::get_info())) {
+	not_found();
 }
 
 $globals['path'] = array_slice($path, 2);
