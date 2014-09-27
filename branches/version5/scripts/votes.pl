1b8082c19ae275e874557d5a130c0c4ae0e17662
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2014-09-27 10:25:13 +0200

    "Best practices"
    Sé que es un poco académico, pero es buena costumbre poner siempre use strict y warnings. 
    También sé que esto es para uso interno, pero la regexp que usas para que se usen sólo decimales podría saltarse quizás con Unicode... No lo sé, pero es mejor extraer en vez de borrar.

--- branches/version5/scripts/votes.pl
+++ branches/version5/scripts/votes.pl
@@ -3,11 +3,14 @@
 
 use DBI;
 
+use strict;
+use warnings;
+
 my $id = shift;
 
 $id =~ s/[^0-9]//g;
 
-$sql = "select user_login, vote_date, vote_value from users, votes where vote_type='links' and vote_link_id = $id and user_id=vote_user_id order by vote_date asc";
+my $sql = "select user_login, vote_date, vote_value from users, votes where vote_type='links' and vote_link_id = $id and user_id=vote_user_id order by vote_date asc";
 
 
 my $dbh = DBI->connect ('DBI:mysql:meneame', 'meneame', '');
