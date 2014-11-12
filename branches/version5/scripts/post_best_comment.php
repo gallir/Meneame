94f27e9cf30a961b7839a51504ad9f3543ed1e93
Ricardo Galli <gallir@gmail.com>
2014-11-12 01:21:02 +0000

    Post best comment to Twitter
    git-svn-id: file:///home/gallir/meneame/svn/src@4412 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version5/scripts/post_best_comment.php
@@ -0,0 +1,58 @@
+#! /usr/bin/env php
+<?php
+
+// Check which hostname server we run for, for example: mnm, emnm, etc.
+$site_name = $argv[2];
+
+include(dirname(__FILE__).'/../config.php');
+include(mnminclude.'external_post.php');
+
+$my_id = SitesMgr::get_id($site_name);
+
+if (! $my_id > 0) {
+    syslog(LOG_INFO, "Meneame, ".basename(__FILE__)." site not found $site_name");
+    echo "No site id found\n";
+    die;
+}
+
+SitesMgr::__init($my_id);
+
+syslog(LOG_INFO, "Meneame, running ".basename(__FILE__)." for $site_name");
+
+$info = SitesMgr::get_info();
+$properties = SitesMgr::get_extended_properties();
+
+if (intval($argv[1]) > 0) {
+	$hours = intval($argv[1]);
+} else {
+	$hours = 1;
+}
+
+$now = intval(time()/60) * 60;
+$min_karma = $globals['comment_highlight_karma'];
+
+$sql = "select comment_id, karma, comment_karma*(1-($now-unix_timestamp(comment_date))*0.8/($hours*3600)) as value from comments, sub_statuses where id = $my_id AND status in ('published') AND comment_date > date_sub(now(), interval $hours hour) and LENGTH(comment_content) > 120 and comment_karma > $min_karma and comment_link_id = link order by value desc limit 1";
+
+$res = $db->get_row($sql);
+if (! $res) {
+	exit(0);
+}
+
+$comment = Comment::from_db($res->comment_id);
+if (! $comment) {
+	exit(2);
+}
+
+$image = false;
+if ($comment->media_size > 0) {
+	$media = new Upload('comment', $comment->id);
+	if ($media->read()) {
+		$image = $media->pathname();
+		$maxlen -= 24;
+	}
+}
+
+$url = $globals[scheme].'//'.get_server_name().$comment->get_relative_individual_permalink();
+syslog(LOG_INFO, "Meneame, posting comment $url");
+twitter_post($properties, _('#comentario').' '.$comment->content, $url, $image);
+
