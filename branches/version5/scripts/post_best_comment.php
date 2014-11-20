9720adb3c428f19253545f15f0f687916064b81a
Ricardo Galli <gallir@gmail.com>
2014-11-20 23:24:36 +0000

    tuning
    git-svn-id: file:///home/gallir/meneame/svn/src@4423 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version5/scripts/post_best_comment.php
+++ branches/version5/scripts/post_best_comment.php
@@ -35,9 +35,14 @@ if ($previous) {
 }
 
 $now = intval(time()/60) * 60;
+$coef = 0.8;
 $min_karma = $globals['comment_highlight_karma'] * 2;
+$min_value = $globals['comment_highlight_karma'];
 
-$sql = "select comment_id, karma, comment_karma*(1-($now-unix_timestamp(comment_date))*0.8/($hours*3600)) as value from comments, sub_statuses where id = $my_id AND status in ('published') AND comment_date > date_sub(now(), interval $hours hour) and LENGTH(comment_content) > 140 and comment_karma > $min_karma AND comment_link_id = link $extra order by value desc limit 1";
+$sql_value =   "comment_karma*(1-($now-unix_timestamp(comment_date))*$coef/($hours*3600)) as value";
+$where_value = "comment_karma*(1-($now-unix_timestamp(comment_date))*$coef/($hours*3600)) > $min_value";
+
+$sql = "select comment_id, karma, $sql_value from comments, sub_statuses where id = $my_id AND status in ('published') AND comment_date > date_sub(now(), interval $hours hour) and LENGTH(comment_content) > 300 and comment_karma > $min_karma AND $where_value AND comment_link_id = link $extra order by value desc limit 1";
 
 $res = $db->get_row($sql);
 if (! $res) {
@@ -59,24 +64,23 @@ if ($comment->media_size > 0) {
 }
 
 
-
 $url = $globals[scheme].'//'.get_server_name().$comment->get_relative_individual_permalink();
 syslog(LOG_INFO, "Meneame, posting comment $url");
 
-if (twitter_post($properties, '&#x1f4ac; '.$comment->content, $url, $image)) {
-	//  Store in cache
-	if ($previous) {
-		$ids = explode(',',$previous);
-		if (count($ids) > 5) {
-			array_shift($ids);
-		}
-	} else {
-		$ids = array();
+//  Store in cache
+if ($previous) {
+	$ids = explode(',',$previous);
+	if (count($ids) > 10) {
+		array_shift($ids);
 	}
-
-	$ids[] = $comment->id;
-	$previous = implode(',', $ids);
-	Annotation::store_text($key, $previous, time() + 86400);
+} else {
+	$ids = array();
 }
-	
+
+$ids[] = $comment->id;
+$previous = implode(',', $ids);
+Annotation::store_text($key, $previous, time() + 86400);
+
+twitter_post($properties, '&#x1f4ac; '.$comment->content, $url, $image);
+
 
