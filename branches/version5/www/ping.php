6d114a6b0caf0758e26fefe3f8eddd8278fdd75c
Ricardo Galli <gallir@gmail.com>
2014-12-25 10:20:13 +0000

    Improved ping, check also form memcache availability.
    git-svn-id: file:///home/gallir/meneame/svn/src@4453 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version5/www/ping.php
+++ branches/version5/www/ping.php
@@ -7,17 +7,39 @@ $globals['force_ssl'] = false;
 
 header("Content-Type: text/plain");
 
-// Chech for cache typical dirs
-for ($i=0; $i<10; $i++) {
-	if ( is_dir($globals['cache_dir'].'/0'.$i)) {
-		if (empty($globals['maintenance'])) {
-			echo "pong (" . SitesMgr::my_id() . ")\n"; // Force a DB access
-		} else {
-			echo "pong\n"; 
+
+if (empty($globals['maintenance'])) {
+	// Check cache dir
+	if ( ! is_dir($globals['cache_dir'])) {
+		ping_error('cache directory not available');
+	}
+	if ( ! is_writeable($globals['cache_dir'])) {
+		ping_error('cache directory not writeble');
+	}
+
+	// Check access to DB
+	if ( ! SitesMgr::my_id() ) { // Force DB access
+		ping_error('DB not available');
+	}
+
+	// Check memcache
+	if ( memcache_menabled() ) {
+		$data = array(1, 2, 3);
+		memcache_madd('ping', $data, 10);
+		$result = memcache_mget('ping');
+		if (! $result || $data != $result) {
+			ping_error('memcache failed');
 		}
-		die;
 	}
 }
-header('HTTP/1.1 500 Server error');
 
-?>
+echo "pong\n";
+
+function ping_error($log) {
+	header('HTTP/1.1 500 Server error');
+	if (! empty($log)) {
+		syslog(LOG_INFO, "ping: $log");
+	}
+	die;
+}
+
