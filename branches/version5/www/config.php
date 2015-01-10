23be9b71b179393e583bf5d5e5e86092a7744f09
Ricardo Galli <gallir@gmail.com>
2015-01-10 22:42:07 +0000

    New transparent spinner.
    git-svn-id: file:///home/gallir/meneame/svn/src@4494 cbe1a1ec-7179-4919-8e32-4551e8279a00

--- branches/version5/www/config.php
+++ branches/version5/www/config.php
@@ -364,8 +364,8 @@ $globals['css_main'] = 'mnm.php';
 $globals['css_webfonts'] = "//fonts.googleapis.com/css?family=Roboto:400";
 //$globals['css_color'] = 'css/es/mnmcol17.css';
 $globals['html_main'] = 'html1.php';
-$globals['thread_padding_percent'] = 2.5; // Padding for threaded comments
-$globals['thread_padding_max_percent'] = 30; // Don't pad more than this %
+$globals['thread_padding_percent'] = 3; // Padding for threaded comments
+$globals['thread_padding_max_percent'] = 20; // Don't pad more than this %
 
 // Store access stats
 $globals['save_pageloads'] = false;
