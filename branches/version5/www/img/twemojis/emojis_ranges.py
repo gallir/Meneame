98c69f9295d2eb009d99fba4b84db5d69315ac0e
Ricardo Galli <gallir@gmail.com>
2015-03-12 21:48:39 +0000

    Added twemojis prepared by @damianvila.
    git-svn-id: file:///home/gallir/meneame/svn/src@4585 cbe1a1ec-7179-4919-8e32-4551e8279a00

---
+++ branches/version5/www/img/twemojis/emojis_ranges.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+
+""" Prinmt the ranges of hexadecimal values found inm *.png
+    Used to build the PHO funcion mb_encode_numericentity()
+"""
+
+import glob
+import os
+
+hexs = []
+
+def print_line(first, last):
+		mask = 'f' * (len(hex(last))-2)
+		print "0x%x, 0x%x, 0, 0x%s," % (first, last, mask)
+
+for f in glob.glob("*.png"):
+	name, ext = os.path.splitext(f)
+	try:
+		number = int(name, 16)
+	except ValueError:
+		continue
+
+	hexs.append(number)
+
+
+hexs = sorted(hexs)
+first = current = previous = None
+current = None
+for h in hexs:
+	if not first:
+		first = previous = h
+		continue
+
+	if previous + 1 != h:
+		print_line(first, previous)
+		first = h
+
+	previous = h
+
+print_line(first, previous)
+
