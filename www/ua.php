788cf7a4f77454cf0890f2c5d0d7c85ff1e49fb5
Ricardo Galli <gallir@gmail.com>
2015-04-30 19:36:32 +0000

    testing small changes

---
+++ www/ua.php
@@ -0,0 +1,9 @@
+<?php
+$globals['skip_check_ip_noaccess'] = true;
+include('./config.php');
+
+echo "IP: " .$globals['user_ip'] . "<br/>";
+echo "User-Agent: " .$_SERVER['HTTP_USER_AGENT'] . "<br/>";
+echo "Mobile: ";
+if ($globals['mobile']) echo "Yes";
+else echo  "No";
