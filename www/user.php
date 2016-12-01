e068ba3242a43126779e89b7ceefbd335c718a00
Félix Gómez López <felix.gomez.lopez@gmail.com>
2016-12-01 17:18:51 +0100

    redesigned user subs zone

--- www/user.php
+++ www/user.php
@@ -587,46 +587,85 @@ function do_subs() {
 
 	$sql = "select subs.* from subs, prefs where pref_user_id = $user->id and pref_key = 'sub_follow' and subs.id = pref_value order by name asc";
 	$subs = $db->get_results($sql);
-	$subs_followers_counter = $db->get_results("select subs.id, count(*) as c from subs, prefs where pref_key = 'sub_follow' and subs.id = pref_value group by subs.id order by c desc;");
+	$followers = $db->get_results("select subs.id, count(*) as c from subs, prefs where pref_key = 'sub_follow' and subs.id = pref_value group by subs.id order by c desc;");
 
-	if ($subs) {
+	$subscribed_subs = array();
 
-		foreach ($subs as $s) {
-			foreach ($subs_followers_counter as $sub_counter) {
-				if ($s->id == $sub_counter->id) {
-					$s->followers = $sub_counter->c;
+		foreach ($subs as $sub) {
+			$sub->site_info = SitesMgr::get_info($sub->id);
+
+			// Check if the sub has a logo and calculate the width
+			if ($sub->site_info->media_id > 0 && $sub->site_info->media_dim1 > 0 && $sub->site_info->media_dim2 > 0) {
+				$r = $sub->site_info->media_dim1/$sub->site_info->media_dim2;
+				if ( $globals['mobile']) {
+					$sub->site_info->logo_height = $globals['media_sublogo_height_mobile'];
+				} else {
+					$sub->site_info->logo_height = $globals['media_sublogo_height'];
+				}
+				$sub->site_info->logo_width = round($r * $sub->site_info->logo_height);
+				$sub->site_info->logo_url = Upload::get_cache_relative_dir($sub->site_info->id).'/media_thumb-sub_logo-'.$sub->site_info->id.'.'.$sub->site_info->media_extension.'?'.$sub->site_info->media_date;
+			}
+
+			$sub->followers = 0;
+
+			foreach ($followers as $row) {
+				if ($sub->id == $row->id) {
+					$sub->followers = $row->c;
 				}
 			}
-			if (!isset($s->followers)) $s->followers=0;
+
+			$subscribed_subs[] = $sub;
 		}
 
-		$title = _('suscripciones');
-		Haanga::Load('subs_simple.html', compact('title', 'subs'));
-	}
-
+	$title_subscriptions = _('Suscripciones');
+	$subscribed_subs = $subs;
 
 	if ($current_user->admin && $user->id == $current_user->user_id) {
 		$sql = "select subs.* from subs where subs.sub = 1 and (subs.owner = $user->id or subs.owner = 0)";
 	} else {
 		$sql = "select subs.* from subs where subs.sub = 1 and subs.owner = $user->id";
 	}
-	$subs = $db->get_results($sql);
+	$ownwed_subs = $db->get_results($sql);
 
-	if ($subs) {
+	$owned_subs = array();
 
-		foreach ($subs as $s) {
-			foreach ($subs_followers_counter as $sub_counter) {
-				if ($s->id == $sub_counter->id) {
-					$s->followers = $sub_counter->c;
-				}
+	$ids_subs = array_map(function($row) {
+		return (int)$row->id;
+	}, $ownwed_subs);
+
+	$followers = $db->get_results('SELECT subs.id, COUNT(*) AS c FROM subs, prefs WHERE subs.id IN ('.implode(',', $ids_subs).') AND pref_key = "sub_follow" AND subs.id = pref_value GROUP BY subs.id ORDER BY c DESC;');
+
+	foreach ($ownwed_subs as $sub) {
+
+		$sub->site_info = SitesMgr::get_info($sub->id);
+
+		// Check if the sub has a logo and calculate the width
+		if ($sub->site_info->media_id > 0 && $sub->site_info->media_dim1 > 0 && $sub->site_info->media_dim2 > 0) {
+			$r = $sub->site_info->media_dim1/$sub->site_info->media_dim2;
+			if ( $globals['mobile']) {
+				$sub->site_info->logo_height = $globals['media_sublogo_height_mobile'];
+			} else {
+				$sub->site_info->logo_height = $globals['media_sublogo_height'];
 			}
-			if (!isset($s->followers)) $s->followers=0;
+			$sub->site_info->logo_width = round($r * $sub->site_info->logo_height);
+			$sub->site_info->logo_url = Upload::get_cache_relative_dir($sub->site_info->id).'/media_thumb-sub_logo-'.$sub->site_info->id.'.'.$sub->site_info->media_extension.'?'.$sub->site_info->media_date;
 		}
 
-		$title = _('subs de') . " $user->username";
-		if ($current_user->user_id > 0 && $user->id == $current_user->user_id && SitesMgr::can_edit(0)) $can_edit = true;
-		else $can_edit = false;
+		$sub->followers = 0;
 
-		Haanga::Load('subs.html', compact('title', 'subs', 'can_edit'));
+		foreach ($followers as $row) {
+			if ($sub->id == $row->id) {
+				$sub->followers = $row->c;
+			}
+		}
+
+		$owned_subs[] = $sub;
 	}
+
+	$title_owned_subs = _('Subs de') . " $user->username";
+	if ($current_user->user_id > 0 && $user->id == $current_user->user_id && SitesMgr::can_edit(0)) $can_edit = true;
+	else $can_edit = false;
+
+	Haanga::Load('subs_user.html', compact('title_subscriptions', 'subscribed_subs', 'title_owned_subs', 'owned_subs', 'can_edit'));
+
 }
