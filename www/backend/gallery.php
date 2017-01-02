9d7867e220274d6128c832f0646300036b7d8e78
Lito <lito@eordes.com>
2017-01-02 18:46:28 +0100

    Fixed SELECT into loop in gallery

--- www/backend/gallery.php
+++ www/backend/gallery.php
@@ -3,66 +3,118 @@
 // Ricardo Galli <gallir at uib dot es>.
 // It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
 // You can get copies of the licenses here:
-// 		http://www.affero.org/oagpl.html
+//      http://www.affero.org/oagpl.html
 // AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
 
 include('../config.php');
 
 $user_id = intval($_GET['user']);
 
-$limit = 250; 
+$limit = 250;
 $show_all = false;
 
 switch ($_GET['type']) {
-	case 'comment':
-		$type_in = '("comment")';
-		break;
-	case 'post':
-		$type_in = '("post")';
-		break;
-	case 'all':
-	default:
-		$type_in = '("comment", "post")';
-		break;
+    case 'comment':
+        $type_in = '("comment")';
+        break;
 
+    case 'post':
+        $type_in = '("post")';
+        break;
+
+    default:
+        $type_in = '("comment", "post")';
+        break;
 }
 
 if ($user_id > 0) {
-	$user = "and user = $user_id";
-	if ($current_user->user_id) {
-		$show_all = true;
-	}
-	if ($user_id == $current_user->user_id) {
-		$limit = 5000;
-	} else {
-		$limit = 500;
-	}
-} else $user = '';
+    if ($current_user->user_id) {
+        $show_all = true;
+    }
+
+    if ($user_id == $current_user->user_id) {
+        $limit = 5000;
+    } else {
+        $limit = 500;
+    }
+}
 
 header('Content-Type: text/html; charset=utf-8');
-$media = $db->get_results("select type, id, version, unix_timestamp(date) as date, mime, user as uid, user_login as user from media, users where type in $type_in $user and version = 0 and user_id = media.user order by date desc limit $limit");
+
+$media = $db->get_results(DbHelper::queryPlain('
+    SELECT `id`, `type`, `version`, UNIX_TIMESTAMP(`date`) AS `date`, `mime`, `user` AS `uid`, `user_login` AS `user`
+    FROM `media`, `users`
+    WHERE (
+        `type` IN '.$type_in.'
+        AND `version` = 0
+        AND `user_id` = `media`.`user`
+        '.($user_id ? (' AND `user` = "'.$user_id.'"') : '').'
+    )
+    ORDER BY `date` DESC
+    LIMIT '.$limit.';
+'));
+
+if (empty($media)) {
+    return;
+}
+
+if ($show_all === false) {
+    $media_ids = implode(',', array_map(function($value) {
+        return $value->id;
+    }, $media));
+
+    $comments_karma = array();
+    $comments = $db->get_results(DbHelper::queryPlain('
+        SELECT comment_id, comment_karma
+        FROM comments
+        WHERE comment_id IN ('.$media_ids.');
+    '));
+
+    foreach ($comments as $comment) {
+        $comments_karma[$comment->comment_id] = $comment->comment_karma;
+    }
+
+    $posts_karma = array();
+    $posts = $db->get_results(DbHelper::queryPlain('
+        SELECT post_id, post_karma
+        FROM posts
+        WHERE post_id IN ('.$media_ids.');
+    '));
+
+    foreach ($posts as $post) {
+        $posts_karma[$post->post_id] = $post->post_karma;
+    }
+} else {
+    $comments_karma = $posts_karma = array();
+}
 
 $images = array();
 
-if ($media) {
-	foreach ($media as $image) {
-		if (! $show_all) {
-			switch ($image->type) {
-				case 'comment':
-					$karma = $db->get_var("select comment_karma from comments where comment_id = $image->id");
-					break;
-				case 'post':
-					$karma = $db->get_var("select post_karma from posts where post_id = $image->id");
-					break;
-				default:
-					$karma = 0;
-			}
-		}
-			
-		if ($show_all || $karma > -10) {
-			$image->url = Upload::get_url($image->type, $image->id, $image->version, $image->date, $image->mime);
-			$images[] = $image;
-		}
-	}
+foreach ($media as $image) {
+    $karma = null;
+
+    if ($show_all === false) {
+        switch ($image->type) {
+            case 'comment':
+                $karma = isset($comments_karma[$image->id]) ? $comments_karma[$image->id] : 0;
+                break;
+
+            case 'post':
+                $karma = isset($posts_karma[$image->id]) ? $posts_karma[$image->id] : 0;
+                break;
+
+            default:
+                $karma = 0;
+                break;
+        }
+    }
+
+    if ($show_all || ($karma > -10)) {
+        $image->url = Upload::get_url($image->type, $image->id, $image->version, $image->date, $image->mime);
+        $images[] = $image;
+    }
 }
-if ($images) Haanga::Load("backend/gallery.html", compact('images'));
+
+if ($images) {
+    Haanga::Load("backend/gallery.html", compact('images'));
+}
