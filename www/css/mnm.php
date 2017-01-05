84527ea70c3df01ae96611af9422385f7ad19582
Lito <lito@eordes.com>
2017-01-05 12:53:39 +0100

    Pretty CSS

--- www/css/mnm.php
+++ www/css/mnm.php
@@ -1,10 +1,10 @@
 <?php
 include('../config.php');
+
 header('Content-Type: text/css; charset=utf-8');
 header('Cache-Control: public, max-age=864000');
 header('Expires: ' . gmdate('r', $globals['now'] + 864000));
 header('Last-Modified: ' .  gmdate('D, d M Y H:i:s', max(filemtime('mnm.css'), filemtime('handheld.css')) ) . ' GMT');
-
 
 Haanga::Load('css/titatoggle-dist-min.css');
 Haanga::Load('css/colorbox.css');
