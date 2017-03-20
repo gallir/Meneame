61fc5d3c315814ab6a40781916330503a588c2db
Lito <lito@eordes.com>
2017-03-20 12:05:12 +0100

    Improved user profile

--- www/user/shaken.php
+++ www/user/shaken.php
@@ -37,17 +37,13 @@ foreach ($links as $linkdb) {
         continue;
     }
 
-    echo '<div style="max-width: 60em">';
-
-    $link->print_summary('short', 0, false);
-
     if ($linkdb->vote_value < 0) {
-        echo '<div class="box" style="z-index:1;margin:0 0 -5x 0;background:#FF3333;position:relative;top:-5px;left:85px;width:8em;padding: 1px 1px 1px 1px;border-color:#f00;opacity:0.9;text-align:center;font-size:0.9em;color:#fff;text-shadow: 0 1px 0 #000">';
+        echo '<div class="vote-negative-alert">';
         echo get_negative_vote($linkdb->vote_value);
         echo "</div>\n";
     }
 
-    echo "</div>\n";
+    $link->print_summary('short', 0, false);
 }
 
 do_pages($count, $limit);
