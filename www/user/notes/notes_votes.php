7c553ab96752a2f3000b2fe091717b416ce45253
Félix Gómez <felix.gomez.lopez@gmail.com>
2017-07-05 17:03:32 +0200

    Corrected vote post list

--- www/user/notes/notes_votes.php
+++ www/user/notes/notes_votes.php
@@ -17,16 +17,60 @@ if ($count === 0) {
     return Haanga::Load('user/empty.html');
 }
 
-$posts = $db->get_results('
-    SELECT '.Post::SQL.'
-    INNER JOIN (
-        SELECT post_id
-        '.$query.'
-        ORDER BY post_id DESC
-        LIMIT '.$offset.', '.$limit.'
-    ) AS `id`
-    USING (post_id)
-    ORDER BY post_id DESC;
-', 'Post');
+$posts = $db->get_results('SELECT vote_link_id as id, vote_value as value' . $query  . " ORDER BY vote_date DESC LIMIT $offset,$limit" );
 
-require __DIR__ . '/notes-common.php';
+if (empty($posts)) {
+    return Haanga::Load('user/empty.html');
+}
+
+if ($current_user->user_id > 0) {
+    $post = new Post;
+    $post->author = $current_user->user_id;
+    $post->print_edit_form();
+}
+
+$all_ids = array_map(function ($value) {
+    return $value->id;
+}, $posts);
+
+$pollCollection = new PollCollection;
+$pollCollection->loadFromRelatedIds('post_id', $all_ids);
+
+$ids = array();
+
+echo '<ol class="comments-list">';
+
+$time_read = 0;
+
+foreach ($posts as $p) {
+    echo '<li>';
+    $post = Post::from_db($p->id);
+    $post->poll = $pollCollection->get($post->id);
+    $post->print_summary();
+
+    if ($view == 'notes_votes') {
+        if ($p->value > 0){
+            echo '<i class="fa fa-arrow-circle-up" style="color:green;position:relative;top: -29px;"></i>';
+        } else {
+            echo '<i class="fa fa-arrow-circle-down" style="color:red;position:relative;top: -29px;"></i>';
+        }
+    }
+
+    echo '</li>';
+
+    if ($post->date > $time_read) {
+        $time_read = $post->date;
+    }
+
+    $ids[] = $post->id;
+}
+
+echo "</ol>\n";
+
+Haanga::Load('get_total_answers_by_ids.html', array(
+    'type' => 'post',
+    'ids' => implode(',', $ids),
+));
+
+do_pages($count, $limit);
+
