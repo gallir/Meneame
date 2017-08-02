471d5f60a2bcd2752989384209456bf601f3e562
Lito <lito@eordes.com>
2017-08-02 22:21:02 +0200

    Fixed comment report and some minor style changes

--- www/backend/report_comment.php
+++ www/backend/report_comment.php
@@ -7,15 +7,15 @@
 // AFFERO GENERAL PUBLIC LICENSE is also included in the file called "COPYING".
 
 if (!defined('mnmpath')) {
-    include __DIR__ . '/../config.php';
-    include mnminclude . 'html1.php';
+    include __DIR__.'/../config.php';
+    include mnminclude.'html1.php';
 }
 
 array_push($globals['cache-control'], 'no-cache');
 http_cache();
 
-if (empty($_REQUEST['id']) || !($id = (int)$_REQUEST['id']) || !$current_user->user_id) {
-    die;
+if (empty($_REQUEST['id']) || !($id = (int)$_REQUEST['id']) || empty($current_user->user_id)) {
+    die();
 }
 
 $comment = Comment::from_db($id) or die();
@@ -27,7 +27,7 @@ if ($_POST['process'] === 'newreport') {
 
 if ($_POST['process'] === 'check_can_report') {
     if (!check_security_key($_POST['key'])) {
-        die;
+        die();
     }
 
     $res = check_report($comment, $link_id);
@@ -51,34 +51,34 @@ function check_report($comment, $link_id)
 {
     global $current_user, $globals;
 
-    // Check if current user can report
-    if (!Report::check_report_user_limit()) {
-        return _('has superado el límite de reportes de comentarios<br>(máximo ' . $globals['max_reports_for_comments'] . ' reportes / 24 horas)');
-    }
-
-    // Check for min karma
-    if (!Report::check_min_karma()) {
-        return _('no dispones de karma suficiente para reportar comentarios');
+    // Check that is not a admin comment
+    if ($comment->type === 'admin') {
+        return _('Este comentario no se puede reportar');
     }
 
     // Check if user votes his own comment! :p
     if ($current_user->user_id == $comment->author) {
-        return _('no puedes reportar tu propio comentario');
-    }
-
-    // Check if user has already reported
-    if (Report::already_reported($comment->id)) {
-        return _('Ya has reportado este comentario.');
-    }
-
-    // Check that is not a admin comment
-    if ($comment->type == 'admin') {
-        return _('Este comentario no se puede reportar');
+        return _('No puedes reportar tu propio comentario');
     }
 
     // Check comments closed
     if ($comment->date < $globals['now'] - $globals['time_enabled_comments']) {
         return _('comentarios cerrados');
+    }
+
+    // Check if current user can report
+    if (!Report::check_report_user_limit()) {
+        return _('Has superado el límite de reportes de comentarios<br>(máximo '.$globals['max_reports_for_comments'].' reportes / 24 horas)');
+    }
+
+    // Check for min karma
+    if (!Report::check_min_karma()) {
+        return _('No dispones de karma suficiente para reportar comentarios');
+    }
+
+    // Check if user has already reported
+    if (Report::already_reported($comment->id)) {
+        return _('Ya has reportado este comentario.');
     }
 
     return true;
@@ -89,10 +89,9 @@ function print_edit_form($comment, $link_id)
     global $current_user, $site_key;
 
     $randkey = rand(1000000, 100000000);
+    $key = md5($randkey.$site_key);
 
-    $key = md5($randkey . $site_key);
-
-    echo Haanga::Load("report_new.html", compact(
+    echo Haanga::Load('report_new.html', compact(
         'comment', 'link_id', 'current_user', 'site_key', 'randkey', 'key'
     ), true);
 }
@@ -102,48 +101,48 @@ function check_save_report($comment, $link_id)
     global $site_key, $current_user, $globals, $db;
 
     // Check key
-    if (!$_POST['key'] || ($_POST['key'] != md5($_POST['randkey'] . $site_key))) {
-        return _('petición incorrecta');
-    }
-
-    // Check user equals current user
-    if ($current_user->user_id != $_POST['user_id']) {
-        return _('petición incorrecta');
-    }
-
-    // Check that at least one valid option is selected (report reason)
-    if (!$_POST['report_reason'] || !Report::is_valid_reason($_POST['report_reason'])) {
-        return _('debes seleccionar una opción');
-    }
-
-    // Check if current user can report
-    if (!Report::check_report_user_limit()) {
-        return _('has superado el límite de reportes de comentarios<br>(máximo ' . $globals['max_reports_for_comments'] . ' comentarios / 24 horas)');
-    }
-
-    // Check for min karma
-    if (!Report::check_min_karma()) {
-        return _('no dispones de karma suficiente para reportar comentarios');
-    }
-
-    // Check if user votes his own comment! :p
-    if ($current_user->user_id == $comment->author) {
-        return _('no puedes reportar tu propio comentario');
-    }
-
-    // Check if user has already reported
-    if (Report::already_reported($comment->id)) {
-        return _('Ya has reportado este comentario.');
-    }
-
-    // Check comments closed
-    if ($comment->date < $globals['now'] - $globals['time_enabled_comments']) {
-        return _('comentarios cerrados');
+    if (empty($_POST['key']) || ($_POST['key'] !== md5($_POST['randkey'].$site_key))) {
+        return _('Petición incorrecta');
     }
 
     // Check that is not a admin comment
     if ($comment->type === 'admin') {
         return _('Este comentario no se puede reportar');
+    }
+
+    // Check if user votes his own comment! :p
+    if ($current_user->user_id == $comment->author) {
+        return _('No puedes reportar tu propio comentario');
+    }
+
+    // Check user equals current user
+    if ($current_user->user_id != $_POST['user_id']) {
+        return _('Petición incorrecta');
+    }
+
+    // Check comments closed
+    if ($comment->date < $globals['now'] - $globals['time_enabled_comments']) {
+        return _('Comentarios cerrados');
+    }
+
+    // Check that at least one valid option is selected (report reason)
+    if (empty($_POST['report_reason']) || !Report::is_valid_reason($_POST['report_reason'])) {
+        return _('Debes seleccionar una opción');
+    }
+
+    // Check if current user can report
+    if (!Report::check_report_user_limit()) {
+        return _('Has superado el límite de reportes de comentarios<br>(máximo '.$globals['max_reports_for_comments'].' comentarios / 24 horas)');
+    }
+
+    // Check for min karma
+    if (!Report::check_min_karma()) {
+        return _('No dispones de karma suficiente para reportar comentarios');
+    }
+
+    // Check if user has already reported
+    if (Report::already_reported($comment->id)) {
+        return _('Ya has reportado este comentario.');
     }
 
     // save report
@@ -158,7 +157,7 @@ function check_save_report($comment, $link_id)
         FROM `reports`
         WHERE (
             `report_type` = "'.Report::REPORT_TYPE_LINK_COMMENT.'"
-            AND `report_ref_id` = "'.(int)$report->ref_id.'"
+            AND `report_ref_id` = "'.(int) $report->ref_id.'"
             AND `report_status` <> "'.Report::REPORT_STATUS_PENDING.'"
         );
     ');
@@ -169,7 +168,7 @@ function check_save_report($comment, $link_id)
 
     $success = $report->store();
 
-    if (isset($_POST['ignore']) && (int)$_POST['ignore']) {
+    if (isset($_POST['ignore']) && (int) $_POST['ignore']) {
         User::friend_insert($current_user->user_id, $comment->author, -1);
     }
 
