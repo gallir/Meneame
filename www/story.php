def33d2c21264f43c8dae7e055a5416633c6923e
Lito <lito@eordes.com>
2017-12-26 19:35:26 +0100

    Return to thumbnail as url

--- www/story.php
+++ www/story.php
@@ -330,7 +330,7 @@ if ($link->sponsored && ($sponsor = Sponsor::getByLinkId($link->id))) {
 }
 
 if ($link->has_thumb()) {
-    $globals['thumbnail'] = $link->get_media()->url();
+    $globals['thumbnail'] = $link->media_url;
 }
 
 $globals['description'] = text_to_summary($link->content, 160);
